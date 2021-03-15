6af3286ba261bbfec6408334814f8ed311f59d0c
Reviath <28-Reviath@users.noreply.git.randomchars.net>
2021-03-15 20:40:08 +0000

    Update client/bot.go

--- client/bot.go
+++ client/bot.go
@@ -5,7 +5,7 @@ import (
 
 	"../config"
 	"github.com/bwmarrin/discordgo"
-	Handler "git.randomchars.net/Reviath/remilia-scarlet-command-handler"
+	Handler "git.randomchars.net/Reviath/handlers"
 )
 
 var BotUsername string
@@ -21,19 +21,13 @@ func Start() {
 		return
 	}
 
-	handler := Handler.New([]string{config.BotPrefix}, []string{config.Owner, "808393372448325672"}, true, true, goBot.StateEnabled)
-
-	goBot.AddHandler(handler.MessageHandler)
-
-	handler.SetOnErrorFunc(func(context Handler.Context, command *Handler.Command, content []string, err error) {
-		fmt.Printf("An error occurred for command \"%s\": \"%s\".\n", command.Name, err.Error())
-	})
-
 	u, err := goBot.User("@me")
 
 	if err != nil {
 		fmt.Println(err.Error())
 	}
+
+	goBot.AddHandler(Handler.MessageHandler)
 
 	BotID = u.ID
 	BotUsername = u.Username
