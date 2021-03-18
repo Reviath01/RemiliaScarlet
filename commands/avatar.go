615c86092ef75c74c91647dc094e6b568aa4c941
Reviath <28-Reviath@users.noreply.git.randomchars.net>
2021-03-18 09:41:35 +0000

    You can get users avatar with user id.

--- commands/avatar.go
+++ commands/avatar.go
@@ -4,6 +4,7 @@ import (
 	ctx "git.randomchars.net/Reviath/handlers/Context"
 	"github.com/bwmarrin/discordgo"
     embedutil "git.randomchars.net/Reviath/embed-util"
+    "strings"
 )
 
 type Avatar struct {
@@ -11,9 +12,18 @@ type Avatar struct {
 }
 
 func (a Avatar) Execute(ctx ctx.Ctx, session *discordgo.Session) error {
-    avatarembed := embedutil.NewEmbed().
-    SetColor(0xff1000).
-    SetImage(ctx.Author().AvatarURL("avatar")).MessageEmbed
-	_, err := session.ChannelMessageSendEmbed(ctx.Channel().ID, avatarembed)
-	return err
+    u, err := session.User(strings.Join(ctx.Args()," "))
+    if err == nil {
+        avatarembed2 := embedutil.NewEmbed().
+            SetColor(0xff1000).
+            SetImage(u.AvatarURL("avatar")).MessageEmbed
+	    _, err2 := session.ChannelMessageSendEmbed(ctx.Channel().ID, avatarembed2)
+	return err2
+    } else {
+        avatarembed := embedutil.NewEmbed().
+            SetColor(0xff1000).
+            SetImage(ctx.Author().AvatarURL("avatar")).MessageEmbed
+	    _, err := session.ChannelMessageSendEmbed(ctx.Channel().ID, avatarembed)
+	    return err
+    }
 }
