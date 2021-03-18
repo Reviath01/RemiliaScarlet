2f8c82acc50a8fb3461509863e797d8536a1f2fc
Reviath <28-Reviath@users.noreply.git.randomchars.net>
2021-03-18 09:52:02 +0000

    You can get users avatar with mentioning.

--- commands/avatar.go
+++ commands/avatar.go
@@ -19,11 +19,20 @@ func (a Avatar) Execute(ctx ctx.Ctx, session *discordgo.Session) error {
             SetImage(u.AvatarURL("avatar")).MessageEmbed
 	    _, err2 := session.ChannelMessageSendEmbed(ctx.Channel().ID, avatarembed2)
 	return err2
-    } else {
+    } else {	
+        u2, err := session.User(strings.Join(ctx.Args()," ")[3:][:18])
+        if err == nil {
         avatarembed := embedutil.NewEmbed().
             SetColor(0xff1000).
+            SetImage(u2.AvatarURL("avatar")).MessageEmbed
+	    _, err3 := session.ChannelMessageSendEmbed(ctx.Channel().ID, avatarembed)
+	    return err3
+        } else {
+        avatarembed3 := embedutil.NewEmbed().
+            SetColor(0xff1000).
             SetImage(ctx.Author().AvatarURL("avatar")).MessageEmbed
-	    _, err := session.ChannelMessageSendEmbed(ctx.Channel().ID, avatarembed)
-	    return err
+	    _, err4 := session.ChannelMessageSendEmbed(ctx.Channel().ID, avatarembed3)
+	        return err4
+        }
     }
 }
