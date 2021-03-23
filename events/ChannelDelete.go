071017f96b998527cef1259567327e0a8f25b03b
Reviath <Reviath-28@noreply.users.git.randomchars.net>
2021-03-23 09:48:22 +0300

    Added channel delete event.

---
+++ events/ChannelDelete.go
@@ -0,0 +1,41 @@
+package events
+
+import (
+	"database/sql"
+
+	embedutil "git.randomchars.net/Reviath/embed-util"
+	"github.com/bwmarrin/discordgo"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func ChannelCreate(s *discordgo.Session, event *discordgo.ChannelDelete) {
+	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/remilia")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer db.Close()
+
+	type Tag struct {
+		channelid string `json:"channelid"`
+	}
+
+	var tag Tag
+
+	err = db.QueryRow("SELECT channelid FROM log WHERE guildid ='" + event.GuildID + "'").Scan(&tag.channelid)
+	if err != nil {
+		return
+	} else {
+		embed := embedutil.NewEmbed().
+			SetTitle("Channel Deleted!").
+			AddField("Channel Name:", event.Channel.Name).
+			AddField("Channel ID:", event.Channel.ID).
+			AddField("Channel Type:", string(event.Channel.Type)).MessageEmbed
+
+		_, err = s.ChannelMessageSendEmbed(tag.channelid, embed)
+		if err != nil {
+			return
+		}
+	}
+}
