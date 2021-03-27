872868e97d4547d63e6cd9bc5505fc56a6c471af
Reviath <28-Reviath@users.noreply.git.randomchars.net>
2021-03-27 21:34:55 +0300

    Icon command is ready to use with disabling system.

--- commands/icon.go
+++ commands/icon.go
@@ -4,6 +4,8 @@ import (
 	ctx "git.randomchars.net/Reviath/handlers/Context"
 	"github.com/bwmarrin/discordgo"
     embedutil "git.randomchars.net/Reviath/embed-util"
+	"database/sql"
+    _ "github.com/go-sql-driver/mysql"
 )
 
 type Icon struct {
@@ -11,9 +13,32 @@ type Icon struct {
 }
 
 func (i Icon) Execute(ctx ctx.Ctx, session *discordgo.Session) error {
+	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/remilia")
+
+    if err != nil {
+        panic(err.Error())
+    }
+
+    defer db.Close()
+
+    type Tag struct {
+        isblocked string `json:"isblocked"`
+    }
+
+    var tag Tag
+
+    err = db.QueryRow("SELECT isblocked FROM disabledcommands WHERE commandname ='icon' AND guildid ='" + ctx.Guild().ID + "'").Scan(&tag.isblocked)
+
+    if err == nil {
+        if tag.isblocked == "True" {
+            _, err = session.ChannelMessageSend(ctx.Channel().ID, "This command is blocked on this guild.")
+            return err
+        }
+    }
+
       iconeembed := embedutil.NewEmbed().
             SetColor(0x00f6ff).
             SetImage(ctx.Guild().IconURL()).MessageEmbed
-	_, err := session.ChannelMessageSendEmbed(ctx.Channel().ID, iconeembed)
+	_, err = session.ChannelMessageSendEmbed(ctx.Channel().ID, iconeembed)
 	return err
 }
