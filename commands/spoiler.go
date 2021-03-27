1ce620d415d3a86f4bbac271052457497a5236ed
Reviath <28-Reviath@users.noreply.git.randomchars.net>
2021-03-27 21:50:57 +0300

    Spoiler command is ready to use with disabling system.

--- commands/spoiler.go
+++ commands/spoiler.go
@@ -5,13 +5,38 @@ import (
 	ctx "git.randomchars.net/Reviath/handlers/Context"
 	"github.com/bwmarrin/discordgo"
     embedutil "git.randomchars.net/Reviath/embed-util"
+    "database/sql"
+    _ "github.com/go-sql-driver/mysql"
 )
 
 type Spoiler struct {
 
 }
 
-func (e Spoiler) Execute(ctx ctx.Ctx, session *discordgo.Session) error {
+func (s Spoiler) Execute(ctx ctx.Ctx, session *discordgo.Session) error {
+	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/remilia")
+
+    if err != nil {
+        panic(err.Error())
+    }
+
+    defer db.Close()
+
+    type Tag struct {
+        isblocked string `json:"isblocked"`
+    }
+
+    var tag Tag
+
+    err = db.QueryRow("SELECT isblocked FROM disabledcommands WHERE commandname ='spoiler' AND guildid ='" + ctx.Guild().ID + "'").Scan(&tag.isblocked)
+
+    if err == nil {
+        if tag.isblocked == "True" {
+            _, err = session.ChannelMessageSend(ctx.Channel().ID, "This command is blocked on this guild.")
+            return err
+        }
+    }
+
     if strings.Join(ctx.Args()," ") == "" {
     _, err := session.ChannelMessageSend(ctx.Channel().ID, "You need to specify the message.")
     return err
@@ -19,6 +44,6 @@ func (e Spoiler) Execute(ctx ctx.Ctx, session *discordgo.Session) error {
       spoilerembed := embedutil.NewEmbed().
             SetColor(0xe9ff00).
             SetDescription("|| " + strings.Join(ctx.Args()," ") + " ||").MessageEmbed
-	_, err := session.ChannelMessageSendEmbed(ctx.Channel().ID, spoilerembed)
+	_, err = session.ChannelMessageSendEmbed(ctx.Channel().ID, spoilerembed)
 	return err
 }
