464a6e32c59ef9a323be5b26c01938826d40b2a8
Reviath <28-Reviath@users.noreply.git.randomchars.net>
2021-03-27 21:57:11 +0300

    Fixed issue command.

--- commands/issue.go
+++ commands/issue.go
@@ -13,7 +13,7 @@ type Issue struct {
 func (i Issue) Execute(ctx ctx.Ctx, session *discordgo.Session) error {
         issueembed := embedutil.NewEmbed().
             SetColor(0xffa935).
-            SetDescription("Click [here](https://git.randomchars.net/Reviath/remilia-scarlet/-/issues/new) to create an issue on GitLab").
+            SetDescription("Click [here](https://git.randomchars.net/Reviath/RemiliaScarlet/-/issues/new) to create an issue on GitLab").
             AddField("If you don't know how to use GitLab,","You can come to our [guild](https://discord.gg/xqsTvtM2hk) and specify the problem.").MessageEmbed
     _, err := session.ChannelMessageSendEmbed(ctx.Channel().ID, issueembed)
 	return err
