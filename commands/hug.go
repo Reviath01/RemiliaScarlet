2a52f45df0e76ced6744ff86a2585bd2ab23c4b6
Reviath <28-Reviath@users.noreply.git.randomchars.net>
2021-03-27 21:31:47 +0300

    Hug command is ready to use with disabling system.

--- commands/hug.go
+++ commands/hug.go
@@ -5,6 +5,8 @@ import (
 	"github.com/bwmarrin/discordgo"
     embedutil "git.randomchars.net/Reviath/embed-util"
     "strings"
+    "database/sql"
+    _ "github.com/go-sql-driver/mysql"
 )
 
 type Hug struct {
@@ -12,6 +14,29 @@ type Hug struct {
 }
 
 func (h Hug) Execute(ctx ctx.Ctx, session *discordgo.Session) error {
+	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/remilia")
+
+    if err != nil {
+        panic(err.Error())
+    }
+
+    defer db.Close()
+
+    type Tag struct {
+        isblocked string `json:"isblocked"`
+    }
+
+    var tag Tag
+
+    err = db.QueryRow("SELECT isblocked FROM disabledcommands WHERE commandname ='hug' AND guildid ='" + ctx.Guild().ID + "'").Scan(&tag.isblocked)
+
+    if err == nil {
+        if tag.isblocked == "True" {
+            _, err = session.ChannelMessageSend(ctx.Channel().ID, "This command is blocked on this guild.")
+            return err
+        }
+    }
+
     if len(strings.Join(ctx.Args(), " ")) != 22 {
         _, err := session.ChannelMessageSend(ctx.Channel().ID, "You need to specify the user.")
 	        return err
