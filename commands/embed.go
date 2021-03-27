ecfa1706f4e6abb0075802f6dd07b948466ba9b7
Reviath <28-Reviath@users.noreply.git.randomchars.net>
2021-03-27 21:23:42 +0300

    Embed command is ready to use with disabling system.

--- commands/embed.go
+++ commands/embed.go
@@ -5,6 +5,8 @@ import (
 	ctx "git.randomchars.net/Reviath/handlers/Context"
 	"github.com/bwmarrin/discordgo"
     embedutil "git.randomchars.net/Reviath/embed-util"
+    "database/sql"
+    _ "github.com/go-sql-driver/mysql"
 )
 
 type Embed struct {
@@ -12,6 +14,29 @@ type Embed struct {
 }
 
 func (e Embed) Execute(ctx ctx.Ctx, session *discordgo.Session) error {
+	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/remilia")
+
+    if err != nil {
+        panic(err.Error())
+    }
+
+    defer db.Close()
+
+    type Tag struct {
+        isblocked string `json:"isblocked"`
+    }
+
+    var tag Tag
+
+    err = db.QueryRow("SELECT isblocked FROM disabledcommands WHERE commandname ='embed' AND guildid ='" + ctx.Guild().ID + "'").Scan(&tag.isblocked)
+
+    if err == nil {
+        if tag.isblocked == "True" {
+            _, err = session.ChannelMessageSend(ctx.Channel().ID, "This command is blocked on this guild.")
+            return err
+        }
+    }
+
     if strings.Join(ctx.Args()," ") == "" {
     _, err := session.ChannelMessageSend(ctx.Channel().ID, "You need to specify the message.")
     return err
@@ -19,6 +44,6 @@ func (e Embed) Execute(ctx ctx.Ctx, session *discordgo.Session) error {
       embed := embedutil.NewEmbed().
             SetColor(0xc000ff).
             SetDescription(strings.Join(ctx.Args()," ")).MessageEmbed
-	_, err := session.ChannelMessageSendEmbed(ctx.Channel().ID, embed)
+	_, err = session.ChannelMessageSendEmbed(ctx.Channel().ID, embed)
 	return err
 }
