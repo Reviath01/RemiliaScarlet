e2c2cd5e80ee3017974119b0693b314a0d01af26
Reviath <28-Reviath@users.noreply.git.randomchars.net>
2021-03-27 21:54:22 +0300

    Unban command is ready to use with disabling system.

--- commands/unban.go
+++ commands/unban.go
@@ -4,6 +4,8 @@ import (
 	ctx "git.randomchars.net/Reviath/handlers/Context"
 	"github.com/bwmarrin/discordgo"
     "strings"
+    "database/sql"
+    _ "github.com/go-sql-driver/mysql"
 )
 
 type Unban struct {
@@ -11,6 +13,29 @@ type Unban struct {
 }
 
 func (u Unban) Execute(ctx ctx.Ctx, session *discordgo.Session) error {
+	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/remilia")
+
+    if err != nil {
+        panic(err.Error())
+    }
+
+    defer db.Close()
+
+    type Tag struct {
+        isblocked string `json:"isblocked"`
+    }
+
+    var tag Tag
+
+    err = db.QueryRow("SELECT isblocked FROM disabledcommands WHERE commandname ='unban' AND guildid ='" + ctx.Guild().ID + "'").Scan(&tag.isblocked)
+
+    if err == nil {
+        if tag.isblocked == "True" {
+            _, err = session.ChannelMessageSend(ctx.Channel().ID, "This command is blocked on this guild.")
+            return err
+        }
+    }
+
     perms, err := session.State.UserChannelPermissions(ctx.Author().ID, ctx.Channel().ID)
 	if err == nil && (int(perms)&discordgo.PermissionBanMembers == discordgo.PermissionBanMembers) == false {
         _, err := session.ChannelMessageSend(ctx.Channel().ID, "You need ban members permission to run this command.")
