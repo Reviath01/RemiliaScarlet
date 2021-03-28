9396362137620ddbb46c4bfea3cf00718b2569fc
Reviath <28-Reviath@users.noreply.git.randomchars.net>
2021-03-28 18:25:59 +0300

    Updated reset_autorole command.

--- commands/reset_autorole.go
+++ commands/reset_autorole.go
@@ -16,7 +16,12 @@ func (r ResetAutorole) Execute(ctx ctx.Ctx, session *discordgo.Session) error {
     perms, err := session.State.UserChannelPermissions(ctx.Author().ID, ctx.Channel().ID)
 	if err == nil && (int(perms)&discordgo.PermissionAdministrator == discordgo.PermissionAdministrator) == false {
         _, err := session.ChannelMessageSend(ctx.Channel().ID, "You need administrator permission to run this command.")
-	return err
+	
+        if err != nil {
+            return nil
+        }
+
+        return err
     }
 
     db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/remilia")
@@ -38,15 +43,30 @@ func (r ResetAutorole) Execute(ctx ctx.Ctx, session *discordgo.Session) error {
         delete, err := db.Query("DELETE FROM autorole WHERE guildid ='" + ctx.Guild().ID + "'")
         if err != nil {
             _, err = session.ChannelMessageSend(ctx.Channel().ID, "An error occured, please try again.")
+            
+            if err != nil {
+                return nil
+            }
+
             return err
         }
 
         defer delete.Close()
 
         _, err = session.ChannelMessageSend(ctx.Channel().ID, "Successfully reset auto role.")
+        
+        if err != nil {
+            return nil
+        }
+
         return err
     } else {
         _, err = session.ChannelMessageSend(ctx.Channel().ID, "Auto role is not existing, so you can't reset.")
+        
+        if err != nil {
+            return nil
+        }
+
         return err
     }
 }
