7de661b340b6ba19b3199251c860e023e4ddd3e0
Reviath <28-Reviath@users.noreply.git.randomchars.net>
2021-03-28 18:37:41 +0300

    Update slap command

--- commands/slap.go
+++ commands/slap.go
@@ -33,13 +33,23 @@ func (s Slap) Execute(ctx ctx.Ctx, session *discordgo.Session) error {
     if err == nil {
         if tag.isblocked == "True" {
             _, err = session.ChannelMessageSend(ctx.Channel().ID, "This command is blocked on this guild.")
+            
+            if err != nil {
+                return nil
+            }
+            
             return err
         }
     }
 
     if len(strings.Join(ctx.Args(), " ")) != 22 {
         _, err := session.ChannelMessageSend(ctx.Channel().ID, "You need to specify the user.")
-	        return err
+	    
+        if err != nil {
+            return nil
+        }
+        
+        return err
     }
         u, err := session.User(strings.Join(ctx.Args()," ")[3:][:18])
         if err == nil {
@@ -48,9 +58,19 @@ func (s Slap) Execute(ctx ctx.Ctx, session *discordgo.Session) error {
             SetDescription("<@" + ctx.Author().ID + "> slaps <@" + u.ID + ">").
             SetImage("https://images-ext-1.discordapp.net/external/79sCWyD-TmmyjFxlaQIxAkAANAfV529d-LDHNkGDM0M/%3Fitemid%3D10426943/https/media1.tenor.com/images/b6d8a83eb652a30b95e87cf96a21e007/tenor.gif").MessageEmbed
 	    _, err := session.ChannelMessageSendEmbed(ctx.Channel().ID, embed)
-	    return err
+	    
+        if err != nil {
+            return nil
+        }
+        
+        return err
         } else {
             _, err := session.ChannelMessageSend(ctx.Channel().ID, "You need to specify the user.")
-	        return err
+	        
+            if err != nil {
+                return nil
+            }
+            
+            return err
         }
     }
