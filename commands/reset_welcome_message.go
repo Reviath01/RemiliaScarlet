c5391323212f80a6a6f65c0e655bca5b9ac4fd00
Reviath <28-Reviath@users.noreply.git.randomchars.net>
2021-03-28 18:31:44 +0300

    Update reset_welcome_channel

--- commands/reset_welcome_message.go
+++ commands/reset_welcome_message.go
@@ -15,7 +15,12 @@ func (r ResetWelcomeMessage) Execute(ctx ctx.Ctx, session *discordgo.Session) er
     perms, err := session.State.UserChannelPermissions(ctx.Author().ID, ctx.Channel().ID)
 	if err == nil && (int(perms)&discordgo.PermissionAdministrator == discordgo.PermissionAdministrator) == false {
         _, err := session.ChannelMessageSend(ctx.Channel().ID, "You need administrator permission to run this command.")
-	return err
+	
+        if err != nil {
+            return nil
+        }
+    
+        return err
     }
 
     db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/remilia")
@@ -37,15 +42,30 @@ func (r ResetWelcomeMessage) Execute(ctx ctx.Ctx, session *discordgo.Session) er
         delete, err := db.Query("DELETE FROM welcomemessage WHERE guildid ='" + ctx.Guild().ID + "'")
         if err != nil {
             _, err = session.ChannelMessageSend(ctx.Channel().ID, "An error occured, please try again.")
+            
+            if err != nil {
+                return nil
+            }
+            
             return err
         }
 
         defer delete.Close()
 
         _, err = session.ChannelMessageSend(ctx.Channel().ID, "Successfully reset welcome message.")
+        
+        if err != nil {
+            return nil
+        }
+        
         return err
     } else {
         _, err = session.ChannelMessageSend(ctx.Channel().ID, "Welcome message is not existing, so you can't reset.")
+        
+        if err != nil {
+            return nil
+        }
+        
         return err
     }
 }
