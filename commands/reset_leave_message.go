8d9c1527a201cd0c4f3f88cee13454949b894104
Reviath <28-Reviath@users.noreply.git.randomchars.net>
2021-03-28 18:26:54 +0300

    Update reset_leave_message command

--- commands/reset_leave_message.go
+++ commands/reset_leave_message.go
@@ -15,7 +15,12 @@ func (r ResetLeaveMessage) Execute(ctx ctx.Ctx, session *discordgo.Session) erro
     perms, err := session.State.UserChannelPermissions(ctx.Author().ID, ctx.Channel().ID)
 	if err == nil && (int(perms)&discordgo.PermissionAdministrator == discordgo.PermissionAdministrator) == false {
         _, err := session.ChannelMessageSend(ctx.Channel().ID, "You need administrator permission to run this command.")
-	return err
+	
+        if err != nil {
+            return nil
+        }
+
+        return err
     }
 
     db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/remilia")
@@ -37,15 +42,30 @@ func (r ResetLeaveMessage) Execute(ctx ctx.Ctx, session *discordgo.Session) erro
         delete, err := db.Query("DELETE FROM leavemessage WHERE guildid ='" + ctx.Guild().ID + "'")
         if err != nil {
             _, err = session.ChannelMessageSend(ctx.Channel().ID, "An error occured, please try again.")
+            
+            if err != nil {
+                return nil
+            }
+
             return err
         }
 
         defer delete.Close()
 
         _, err = session.ChannelMessageSend(ctx.Channel().ID, "Successfully reset leave message.")
+        
+        if err != nil {
+            return nil
+        }
+
         return err
     } else {
         _, err = session.ChannelMessageSend(ctx.Channel().ID, "Leave message is not existing, so you can't reset.")
+        
+        if err != nil {
+            return nil
+        }
+
         return err
     }
 }
