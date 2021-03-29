98e56a05e1b32b940c0174d2ce5884b732614ac5
Reviath <28-Reviath@users.noreply.git.randomchars.net>
2021-03-29 08:00:06 +0300

    Updated autorole command.

--- commands/autorole.go
+++ commands/autorole.go
@@ -2,8 +2,6 @@ package commands
 
 import (
 	"database/sql"
-	"strings"
-
 	ctx "git.randomchars.net/Reviath/handlers/Context"
 	"github.com/bwmarrin/discordgo"
 	_ "github.com/go-sql-driver/mysql"
@@ -20,8 +18,11 @@ func (a AutoRole) Execute(ctx ctx.Ctx, session *discordgo.Session) error {
 	return err
     }
 
-    if len(strings.Join(ctx.Args()," ")) == 18 {
-    if err == nil {
+    var args string
+
+    args = ctx.Args()[0]
+
+    if len(args) == 18 {
     db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/remilia")
 
 	if err != nil {
@@ -41,7 +42,7 @@ func (a AutoRole) Execute(ctx ctx.Ctx, session *discordgo.Session) error {
     _, err = session.ChannelMessageSend(ctx.Channel().ID, "Auto role is already existing (to reset, use reset_autorole command).")
     return err
     } else {
-        insert, err := db.Query("INSERT INTO autorole (roleid, guildid) VALUES ('" + strings.Join(ctx.Args()," ") + "', '" + ctx.Guild().ID + "')")
+        insert, err := db.Query("INSERT INTO autorole (roleid, guildid) VALUES ('" + args + "', '" + ctx.Guild().ID + "')")
         if err != nil {
             _, err = session.ChannelMessageSend(ctx.Channel().ID, "An error occured, please try again.")
             
@@ -61,15 +62,8 @@ func (a AutoRole) Execute(ctx ctx.Ctx, session *discordgo.Session) error {
 
 		return err
 	}
-        } else {
-            _, err = session.ChannelMessageSend(ctx.Channel().ID, "You need to specify the role.")
-            if err != nil {
-                return nil
-            }
-            return err
-        }
-    } else {
-        if len(strings.Join(ctx.Args()," ")) == 22 {
+} else {
+        if len(args) == 22 {
     db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/remilia")
 
 	if err != nil {
@@ -92,7 +86,7 @@ func (a AutoRole) Execute(ctx ctx.Ctx, session *discordgo.Session) error {
         }
         return err
     } else {
-        insert, err := db.Query("INSERT INTO autorole (roleid, guildid) VALUES ('" + strings.Join(ctx.Args()," ")[3:][:18] + "', '" + ctx.Guild().ID + "')")
+        insert, err := db.Query("INSERT INTO autorole (roleid, guildid) VALUES ('" + args[3:][:18] + "', '" + ctx.Guild().ID + "')")
         if err != nil {
             _, err = session.ChannelMessageSend(ctx.Channel().ID, "An error occured, please try again.")
             
