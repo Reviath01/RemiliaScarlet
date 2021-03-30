7592e62c5cc8e177d220300f29f4772f31e18ea6
Reviath <28-Reviath@users.noreply.git.randomchars.net>
2021-03-30 22:28:50 +0300

    update embed command.

--- commands/embed.go
+++ commands/embed.go
@@ -24,9 +24,50 @@ func (e Embed) Execute(ctx ctx.Ctx, session *discordgo.Session) error {
 
     type Tag struct {
         isblocked string `json:"isblocked"`
+        lang string `json:"language"`
     }
 
     var tag Tag
+
+    err = db.QueryRow("SELECT language FROM languages WHERE guildid ='" + ctx.Guild().ID + "'").Scan(&tag.lang)
+    if err == nil {
+        if tag.lang == "tr" {
+            err = db.QueryRow("SELECT isblocked FROM disabledcommands WHERE commandname ='embed' AND guildid ='" + ctx.Guild().ID + "'").Scan(&tag.isblocked)
+
+    if err == nil {
+        if tag.isblocked == "True" {
+            _, err = session.ChannelMessageSend(ctx.Channel().ID, "Bu komut bu sunucuda engellenmiş.")
+            
+        if err != nil {
+            return nil
+        }
+
+            return err
+        }
+    }
+
+    if strings.Join(ctx.Args()," ") == "" {
+    _, err := session.ChannelMessageSend(ctx.Channel().ID, "Bir mesaj belirtmelisin.")
+    
+    if err != nil {
+        return nil
+    }
+
+    return err
+    }
+      embed := embedutil.NewEmbed().
+            SetColor(0xc000ff).
+            SetDescription(strings.Join(ctx.Args()," ")).MessageEmbed
+	_, err = session.ChannelMessageSendEmbed(ctx.Channel().ID, embed)
+	
+if err != nil {
+    return nil
+}
+
+    return err
+
+        }
+    }
 
     err = db.QueryRow("SELECT isblocked FROM disabledcommands WHERE commandname ='embed' AND guildid ='" + ctx.Guild().ID + "'").Scan(&tag.isblocked)
 
