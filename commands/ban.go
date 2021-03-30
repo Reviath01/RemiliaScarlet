5cf28f59d549e30b029f72613ec65108200b6d76
Reviath <28-Reviath@users.noreply.git.randomchars.net>
2021-03-30 21:58:56 +0300

    Added Turkish support to ban command.

--- commands/ban.go
+++ commands/ban.go
@@ -23,9 +23,116 @@ func (b Ban) Execute(ctx ctx.Ctx, session *discordgo.Session) error {
 
     type Tag struct {
         isblocked string `json:"isblocked"`
+        lang string `json:"language"`
     }
 
     var tag Tag
+
+    err = db.QueryRow("SELECT language FROM languages WHERE guildid ='" + ctx.Guild().ID + "'").Scan(&tag.lang)
+    if err == nil {
+        if tag.lang == "tr" {
+            err = db.QueryRow("SELECT isblocked FROM disabledcommands WHERE commandname ='ban' AND guildid ='" + ctx.Guild().ID + "'").Scan(&tag.isblocked)
+
+            if err == nil {
+                if tag.isblocked == "True" {
+                    _, err = session.ChannelMessageSend(ctx.Channel().ID, "Bu komut bu sunucuda engellenmiş.")
+        
+                    if err != nil {
+                        return nil
+                    }
+        
+                    return err
+                }
+            }
+        
+            perms, err := session.State.UserChannelPermissions(ctx.Author().ID, ctx.Channel().ID)
+            if err == nil && (int(perms)&discordgo.PermissionBanMembers == discordgo.PermissionBanMembers) == false {
+                _, err := session.ChannelMessageSend(ctx.Channel().ID, "Bu komutu kullanmak için üyeleri yasakla yetkisine sahip olmalısın.")
+            return err
+            }
+        
+            var args string
+            args = ctx.Args()[0]
+        
+            if len(strings.Join(ctx.Args(), " ")) < 1 {
+                _, err = session.ChannelMessageSend(ctx.Channel().ID, "Bir üye belirtmelisin.")
+                if err != nil {
+                    return nil
+                }
+                return err
+            }
+        
+            if len(args) < 19 {
+            u, err := session.User(args)
+            if err == nil {
+                err = session.GuildBanCreate(ctx.Guild().ID, u.ID, 0)
+                if err != nil {
+                    _, err = session.ChannelMessageSend(ctx.Channel().ID, "Yeterli yetkim yok.")
+        
+                    if err != nil {
+                        return nil
+                    }
+        
+                    return err
+                }
+                _, err = session.ChannelMessageSend(ctx.Channel().ID, "Belirtilen kişi banlandı!")
+                
+                if err != nil {
+                    return nil
+                }
+        
+                return err
+            } else {
+                _, err = session.ChannelMessageSend(ctx.Channel().ID, "Bir üye belirtmelisin.")
+                
+                if err != nil {
+                    return nil
+                }
+        
+                return err
+            }
+        } else {
+            if len(args) > 21 {
+             u, err := session.User(args[3:][:18])
+                if err == nil {
+                    err = session.GuildBanCreate(ctx.Guild().ID, u.ID, 0)
+                    if err != nil {
+                        _, err = session.ChannelMessageSend(ctx.Channel().ID, "Yeterli yetkim yok")
+                        
+                        if err != nil {
+                            return nil
+                        }
+        
+                        return err
+                    }
+                    _, err = session.ChannelMessageSend(ctx.Channel().ID, "Belirtilen kişi banlandı!")
+                    
+                    if err != nil {
+                        return nil
+                    }
+        
+                    return err
+                } else {
+                    _, err = session.ChannelMessageSend(ctx.Channel().ID, "Bir üye belirtmelisin.")
+        
+                    if err != nil {
+                        return nil
+                    }
+        
+                   return err
+                }
+            } else {
+                    _, err := session.ChannelMessageSend(ctx.Channel().ID, "Bir üye belirtmelisin.")
+                    
+                    if err != nil {
+                        return nil
+                    }
+        
+                    return err
+                }
+            }
+        }
+    }
 
     err = db.QueryRow("SELECT isblocked FROM disabledcommands WHERE commandname ='ban' AND guildid ='" + ctx.Guild().ID + "'").Scan(&tag.isblocked)
 
