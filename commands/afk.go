544bfa8c929fcecdfc118d606e3374c29ed76176
Reviath <28-Reviath@users.noreply.git.randomchars.net>
2021-03-30 21:10:10 +0300

    Added Turkish support to afk command.

--- commands/afk.go
+++ commands/afk.go
@@ -22,9 +22,46 @@ func (a Afk) Execute(ctx ctx.Ctx, session *discordgo.Session) error {
 
     type Tag struct {
         isblocked string `json:"isblocked"`
+        lang string `json:"language"`
     }
 
     var tag Tag
+
+    err = db.QueryRow("SELECT language FROM languages WHERE guildid ='" + ctx.Guild().ID + "'").Scan(&tag.lang)
+
+    if err == nil {
+        if tag.lang == "tr" {
+
+            err = db.QueryRow("SELECT isblocked FROM disabledcommands WHERE commandname ='afk' AND guildid ='" + ctx.Guild().ID + "'").Scan(&tag.isblocked)
+
+            if err == nil {
+                if tag.isblocked == "True" {
+                    _, err = session.ChannelMessageSend(ctx.Channel().ID, "Bu komut bu sunucuda engellenmiş.")
+                    if err != nil {
+                        return nil
+                    }
+                    return err
+                }
+            }
+        
+            insert, err := db.Query("INSERT INTO afk (isafk, userid) VALUES ('true', '" + ctx.Author().ID + "')")
+        
+            if err != nil {
+                _, err := session.ChannelMessageSend(ctx.Channel().ID, "Bir hata oluştu.")
+                return err
+            }
+        
+               defer insert.Close()
+        
+                _, err = session.ChannelMessageSend(ctx.Channel().ID, "Başarıyla AFK moduna geçtin.")
+                
+                if err != nil {
+                    return nil
+                }
+                
+                return err
+        }
+    }
 
     err = db.QueryRow("SELECT isblocked FROM disabledcommands WHERE commandname ='afk' AND guildid ='" + ctx.Guild().ID + "'").Scan(&tag.isblocked)
 
