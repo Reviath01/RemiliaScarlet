fa053755549f0a138cd4cf8b91ba474e77acd0f1
Reviath <28-Reviath@users.noreply.git.randomchars.net>
2021-03-31 19:30:37 +0300

    Update ping command

--- commands/ping.go
+++ commands/ping.go
@@ -23,9 +23,33 @@ func (p Ping) Execute(ctx ctx.Ctx, session *discordgo.Session) error {
 
     type Tag struct {
         isblocked string `json:"isblocked"`
+        lang string `json:"language"`
     }
 
     var tag Tag
+
+    err = db.QueryRow("").Scan(&tag.lang)
+    if err == nil {
+        if tag.lang == "tr" {
+            err = db.QueryRow("SELECT isblocked FROM disabledcommands WHERE commandname ='ping' AND guildid ='" + ctx.Guild().ID + "'").Scan(&tag.isblocked)
+
+            if err == nil {
+                if tag.isblocked == "True" {
+                    _, err = session.ChannelMessageSend(ctx.Channel().ID, "Bu komut bu sunucuda engellenmiş.")
+                    
+                    if err != nil {
+                        return nil
+                    }
+                    
+                    return err
+                }
+            }
+        
+            _, err = session.ChannelMessageSend(ctx.Channel().ID, "Pong! " + strconv.Itoa(int(session.HeartbeatLatency().Milliseconds())) + "ms")
+            return err
+                
+        }
+    }
 
     err = db.QueryRow("SELECT isblocked FROM disabledcommands WHERE commandname ='ping' AND guildid ='" + ctx.Guild().ID + "'").Scan(&tag.isblocked)
 
