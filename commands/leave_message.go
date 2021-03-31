e1fa2286bf4b3fb79583b21dd1b2e8faa5fc743f
Reviath <28-Reviath@users.noreply.git.randomchars.net>
2021-03-31 19:03:24 +0300

    Update leave_message command.

--- commands/leave_message.go
+++ commands/leave_message.go
@@ -13,6 +13,90 @@ type LeaveMessage struct {
 }
 
 func (l LeaveMessage) Execute(ctx ctx.Ctx, session *discordgo.Session) error {
+	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/remilia")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer db.Close()
+
+	type Tag struct {
+		message string `json:"message"`
+		lang string `json:"language"`
+	}
+
+	var tag Tag
+
+	err = db.QueryRow("SELECT language FROM languages WHERE guildid ='" + ctx.Guild().ID + "'").Scan(&tag.lang)
+	if err == nil {
+		if tag.lang == "tr" {
+			perms, err := session.State.UserChannelPermissions(ctx.Author().ID, ctx.Channel().ID)
+			if err == nil && (int(perms)&discordgo.PermissionAdministrator == discordgo.PermissionAdministrator) == false {
+				_, err := session.ChannelMessageSend(ctx.Channel().ID, "Bu komutu kullanmak için yönetici yetkisine sahip olmalısın.")
+			
+				if err != nil {
+					return nil
+				}
+		
+				return err
+			}
+			var args string
+			if len(strings.Join(ctx.Args(), " ")) < 1 {
+				_, err := session.ChannelMessageSend(ctx.Channel().ID, "Yeni mesajı belirtmelisin.")
+				
+				if err != nil {
+					return nil
+				}
+				
+				return err
+			}
+			args = ctx.Args()[0]
+		
+		
+			if len(args) > 254 {
+				_, err = session.ChannelMessageSend(ctx.Channel().ID, "Mesaj maksimum 255 karakter uzunluğunda olabilir.")
+				
+				if err != nil {
+					return nil
+				}
+		
+				return err
+			}
+		
+			err = db.QueryRow("SELECT message FROM leavemessage WHERE guildid ='" + ctx.Guild().ID + "'").Scan(&tag.message)
+			if err == nil {
+			_, err = session.ChannelMessageSend(ctx.Channel().ID, "Çıkış mesajı zaten ayarlanmış, reset'lemek için reset_leave_message komutunu kullan.")
+			
+			if err != nil {
+				return nil
+			}
+		
+			return err
+			} else {
+				insert, err := db.Query("INSERT INTO leavemessage (message, guildid) VALUES ('" + args + "', '" + ctx.Guild().ID + "')")
+				if err != nil {
+					_, err = session.ChannelMessageSend(ctx.Channel().ID, "Bir hata oluştu.")
+					
+					if err != nil {
+						return nil
+					}
+		
+					return err
+				}
+				defer insert.Close()
+		
+				_, err = session.ChannelMessageSend(ctx.Channel().ID, "Başarıyla çıkış mesajı ayarlandı.")
+				
+				if err != nil {
+					return nil
+				}
+		
+				return err
+				}
+		}
+	}
+
     perms, err := session.State.UserChannelPermissions(ctx.Author().ID, ctx.Channel().ID)
 	if err == nil && (int(perms)&discordgo.PermissionAdministrator == discordgo.PermissionAdministrator) == false {
         _, err := session.ChannelMessageSend(ctx.Channel().ID, "You need administrator permission to run this command.")
@@ -45,20 +129,6 @@ func (l LeaveMessage) Execute(ctx ctx.Ctx, session *discordgo.Session) error {
 
 		return err
 	}
-
-    db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/remilia")
-
-	if err != nil {
-		panic(err.Error())
-	}
-
-	defer db.Close()
-
-	type Tag struct {
-		message string `json:"message"`
-	}
-
-	var tag Tag
 
 	err = db.QueryRow("SELECT message FROM leavemessage WHERE guildid ='" + ctx.Guild().ID + "'").Scan(&tag.message)
 	if err == nil {
