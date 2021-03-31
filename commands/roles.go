5623c6f29e3139cb9f0ff575ecc385e9208d93ac
Reviath <28-Reviath@users.noreply.git.randomchars.net>
2021-03-31 21:10:58 +0300

    Update roles command

--- commands/roles.go
+++ commands/roles.go
@@ -25,9 +25,45 @@ func (r Roles) Execute(ctx ctx.Ctx, session *discordgo.Session) error {
 
     type Tag struct {
         isblocked string `json:"isblocked"`
+        lang string `json:"language"`
     }
 
     var tag Tag
+
+    err = db.QueryRow("SELECT language FROM languages WHERE guildid ='" + ctx.Guild().ID + "'").Scan(&tag.lang)
+    if err == nil {
+        if tag.lang == "tr" {
+            err = db.QueryRow("SELECT isblocked FROM disabledcommands WHERE commandname ='roles' AND guildid ='" + ctx.Guild().ID + "'").Scan(&tag.isblocked)
+
+            if err == nil {
+                if tag.isblocked == "True" {
+                    _, err = session.ChannelMessageSend(ctx.Channel().ID, "Bu komut bu sunucuda engellenmiş.")
+                    
+                    if err != nil {
+                        return nil
+                    }
+                    
+                    return err
+                }
+            }
+        
+            for _, i := range ctx.Guild().Roles {
+                roles += "<@&" + i.ID + ">" + ", \n"
+            }
+        
+            embed := embedutil.NewEmbed().
+                AddField("Roller:", roles).MessageEmbed
+                
+            _, err = session.ChannelMessageSendEmbed(ctx.Channel().ID, embed)
+            
+            if err != nil {
+                return nil
+            }
+            
+            return err
+        
+        }
+    }
 
     err = db.QueryRow("SELECT isblocked FROM disabledcommands WHERE commandname ='roles' AND guildid ='" + ctx.Guild().ID + "'").Scan(&tag.isblocked)
 
