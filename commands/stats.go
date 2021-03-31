06d01256c5b5682793a2a5a1c6e10956f1588d85
Reviath <28-Reviath@users.noreply.git.randomchars.net>
2021-03-31 21:26:02 +0300

    Update stats command

--- commands/stats.go
+++ commands/stats.go
@@ -39,9 +39,47 @@ func (s Stats) Execute(ctx ctx.Ctx, session *discordgo.Session) error {
 
     type Tag struct {
         isblocked string `json:"isblocked"`
+        lang string `json:"language"`
     }
 
     var tag Tag
+
+    err = db.QueryRow("SELECT language FROM languages WHERE guildid ='" + ctx.Guild().ID + "'").Scan(&tag.lang)
+    if err == nil {
+        if tag.lang == "tr" {
+            err = db.QueryRow("SELECT isblocked FROM disabledcommands WHERE commandname ='stats' AND guildid ='" + ctx.Guild().ID + "'").Scan(&tag.isblocked)
+
+            if err == nil {
+                if tag.isblocked == "True" {
+                    _, err = session.ChannelMessageSend(ctx.Channel().ID, "Bu komut bu sunucuda engellenmiş.")
+                    
+                    if err != nil {
+                        return nil
+                    }
+        
+                    return err
+                }
+            }
+        
+            statembed := embedutil.NewEmbed().
+                    SetTitle("İstatistikler").
+                    SetColor(0x00ff03).
+                    AddField("Çalışma Süresi", getDuration(time.Since(startTime))).
+                    AddField("Go Versiyonu", runtime.Version()).
+                    AddField("Discordgo Versiyonu", discordgo.VERSION).
+                    AddField("Sunucular", strconv.Itoa(len(session.State.Guilds))).
+                    AddField("Toplam RAM", strconv.FormatUint(memory.TotalMemory(), 10) + " bytes").
+                    AddField("Goroutine'ler", strconv.Itoa(runtime.NumGoroutine())).MessageEmbed
+                _, err = session.ChannelMessageSendEmbed(ctx.Channel().ID, statembed)
+                
+                if err != nil {
+                    return nil
+                }
+        
+                return err
+        
+        }
+    }
 
     err = db.QueryRow("SELECT isblocked FROM disabledcommands WHERE commandname ='stats' AND guildid ='" + ctx.Guild().ID + "'").Scan(&tag.isblocked)
 
