3f5068a3e4147b7ae6928a889a438c1f1ba8a47b
Reviath <28-Reviath@users.noreply.git.randomchars.net>
2021-03-31 21:17:04 +0300

    Update slap command

--- commands/slap.go
+++ commands/slap.go
@@ -24,10 +24,72 @@ func (s Slap) Execute(ctx ctx.Ctx, session *discordgo.Session) error {
 
     type Tag struct {
         isblocked string `json:"isblocked"`
+        lang string `json:"language"`
     }
 
     var tag Tag
+    err = db.QueryRow("SELECT language FROM languages WHERE guildid ='" + ctx.Guild().ID + "'").Scan(&tag.lang)
+    if err == nil {
+        if tag.lang == "tr" {
+            err = db.QueryRow("SELECT isblocked FROM disabledcommands WHERE commandname ='slap' AND guildid ='" + ctx.Guild().ID + "'").Scan(&tag.isblocked)
 
+            if err == nil {
+                if tag.isblocked == "True" {
+                    _, err = session.ChannelMessageSend(ctx.Channel().ID, "Bu komut bu sunucuda engellenmiş.")
+                    
+                    if err != nil {
+                        return nil
+                    }
+                    
+                    return err
+                }
+            }
+            
+            var args string
+            if len(strings.Join(ctx.Args()," ")) < 1 {
+                _, err := session.ChannelMessageSend(ctx.Channel().ID, "Bir üye belirtmelisin.")
+                
+                if err != nil {
+                    return nil
+                }
+                
+                return err
+            }
+            args = ctx.Args()[0]
+        
+            if len(args) != 22 {
+                _, err := session.ChannelMessageSend(ctx.Channel().ID, "Bir üye belirtmelisin.")
+                
+                if err != nil {
+                    return nil
+                }
+                
+                return err
+            }
+                u, err := session.User(args[3:][:18])
+                if err == nil {
+                embed := embedutil.NewEmbed().
+                    SetColor(0xff1000).
+                    SetDescription("<@" + ctx.Author().ID + ">, <@" + u.ID + "> isimli kişiye vurdu!").
+                    SetImage("https://images-ext-1.discordapp.net/external/79sCWyD-TmmyjFxlaQIxAkAANAfV529d-LDHNkGDM0M/%3Fitemid%3D10426943/https/media1.tenor.com/images/b6d8a83eb652a30b95e87cf96a21e007/tenor.gif").MessageEmbed
+                _, err := session.ChannelMessageSendEmbed(ctx.Channel().ID, embed)
+                
+                if err != nil {
+                    return nil
+                }
+                
+                return err
+                } else {
+                    _, err := session.ChannelMessageSend(ctx.Channel().ID, "Bir üye belirtmelisin.")
+                    
+                    if err != nil {
+                        return nil
+                    }
+                    
+                    return err
+                }        
+        }
+    }
     err = db.QueryRow("SELECT isblocked FROM disabledcommands WHERE commandname ='slap' AND guildid ='" + ctx.Guild().ID + "'").Scan(&tag.isblocked)
 
     if err == nil {
