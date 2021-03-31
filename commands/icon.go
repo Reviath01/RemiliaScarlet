1a72785b859173e4a02763656a12591cd571bd7e
Reviath <28-Reviath@users.noreply.git.randomchars.net>
2021-03-31 08:37:38 +0300

    Update icon command

--- commands/icon.go
+++ commands/icon.go
@@ -23,9 +23,41 @@ func (i Icon) Execute(ctx ctx.Ctx, session *discordgo.Session) error {
 
     type Tag struct {
         isblocked string `json:"isblocked"`
+        lang string `json:"language"`
     }
 
     var tag Tag
+
+    err = db.QueryRow("SELECT language FROM languages WHERE guildid ='" + ctx.Guild().ID + "'").Scan(&tag.lang)
+    if err == nil {
+        if tag.lang == "tr" {
+            err = db.QueryRow("SELECT isblocked FROM disabledcommands WHERE commandname ='icon' AND guildid ='" + ctx.Guild().ID + "'").Scan(&tag.isblocked)
+
+    if err == nil {
+        if tag.isblocked == "True" {
+            _, err = session.ChannelMessageSend(ctx.Channel().ID, "Bu komut bu sunucuda engellenmiş.")
+            
+			if err != nil {
+				return nil
+			}
+			
+			return err
+        }
+    }
+
+      iconeembed := embedutil.NewEmbed().
+            SetColor(0x00f6ff).
+            SetImage(ctx.Guild().IconURL()).MessageEmbed
+	_, err = session.ChannelMessageSendEmbed(ctx.Channel().ID, iconeembed)
+	
+	if err != nil {
+		return nil
+	}
+	
+	return err
+
+        }
+    }
 
     err = db.QueryRow("SELECT isblocked FROM disabledcommands WHERE commandname ='icon' AND guildid ='" + ctx.Guild().ID + "'").Scan(&tag.isblocked)
 
