706652f2b9a4bc7fa1aa0b104b18a641b682a284
Reviath <28-Reviath@users.noreply.git.randomchars.net>
2021-03-31 21:14:59 +0300

    Update settings command

--- commands/settings.go
+++ commands/settings.go
@@ -29,6 +29,7 @@ func (s Settings) Execute(ctx ctx.Ctx, session *discordgo.Session) error {
 		leavemessage string `json:"message"`
 		logid string `json:"channelid"`
 		isblocked string `json:"isblocked"`
+		lang string `json:"language"`
 	}
 
 	var tag Tag
@@ -38,6 +39,86 @@ func (s Settings) Execute(ctx ctx.Ctx, session *discordgo.Session) error {
 	var leavemsg string
 	var welcomemsg string
 	var logchannel string
+
+	err = db.QueryRow("SELECT language FROM languages WHERE guildid ='" + ctx.Guild().ID + "'").Scan(&tag.lang)
+    if err == nil {
+        if tag.lang == "tr" {
+			err = db.QueryRow("SELECT isblocked FROM disabledcommands WHERE commandname ='settings' AND guildid ='" + ctx.Guild().ID + "'").Scan(&tag.isblocked)
+
+			if err == nil {
+				if tag.isblocked == "True" {
+					_, err = session.ChannelMessageSend(ctx.Channel().ID, "Bu komut bu sunucuda engellenmiş.")
+					
+					if err != nil {
+						return nil
+					}
+					
+					return err
+				}
+			}
+		
+			err = db.QueryRow("SELECT channelid FROM welcomechannel WHERE guildid ='" + ctx.Guild().ID + "'").Scan(&tag.welcomechannelid)
+			if err == nil {
+				welcomechannel = "<#" + tag.welcomechannelid + "> (" + tag.welcomechannelid + ")"
+			} else {
+				welcomechannel = "Ayarlanmamış."
+			}
+		
+			err = db.QueryRow("SELECT channelid FROM leavechannel WHERE guildid ='" + ctx.Guild().ID + "'").Scan(&tag.leavechannelid)
+			if err == nil {
+				leavechannel = "<#" + tag.leavechannelid + "> (" + tag.leavechannelid + ")"
+			} else {
+				leavechannel = "Ayarlanmamış."
+			}
+		
+			err = db.QueryRow("SELECT roleid FROM autorole WHERE guildid ='" + ctx.Guild().ID + "'").Scan(&tag.roleid)
+			if err == nil {
+				autorole = "<@&" + tag.roleid + "> (" + tag.roleid + ")"
+			} else {
+				autorole = "Ayarlanmamış."
+			}
+		
+			err = db.QueryRow("SELECT message FROM welcomemessage WHERE guildid ='" + ctx.Guild().ID + "'").Scan(&tag.welcomemessage)
+			if err == nil {
+				welcomemsg = tag.welcomemessage
+			} else {
+				welcomemsg = "Ayarlanmamış."
+			}
+		
+			err = db.QueryRow("SELECT message FROM leavemessage WHERE guildid ='" + ctx.Guild().ID + "'").Scan(&tag.leavemessage)
+			if err == nil {
+				leavemsg = tag.leavemessage
+			} else {
+				leavemsg = "Ayarlanmamış."
+			}
+		
+			err = db.QueryRow("SELECT channelid FROM log WHERE guildid ='" + ctx.Guild().ID + "'").Scan(&tag.logid)
+			if err == nil {
+				logchannel = "<#" + tag.logid + "> (" + tag.logid + ")"
+			} else {
+				logchannel = "Ayarlanmamış."
+			}
+		
+			embed := embedutil.NewEmbed().
+				SetTitle(ctx.Guild().Name + " Settings").
+				AddField("Hoş Geldin Kanalı:", welcomechannel).
+				AddField("Çıkış Kanalı:", leavechannel).
+				AddField("Otorol:", autorole).
+				AddField("Çıkış Mesajı:", leavemsg).
+				AddField("Hoş Geldin Mesajı:", welcomemsg).
+				AddField("Log Kanalı:", logchannel).
+				SetColor(0x00f0ff).MessageEmbed
+				
+			_, err = session.ChannelMessageSendEmbed(ctx.Channel().ID, embed)
+			
+			if err != nil {
+				return nil
+			}
+			
+			return err
+		
+		}
+	}
 
 	err = db.QueryRow("SELECT isblocked FROM disabledcommands WHERE commandname ='settings' AND guildid ='" + ctx.Guild().ID + "'").Scan(&tag.isblocked)
 
