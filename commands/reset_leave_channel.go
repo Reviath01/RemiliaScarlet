6b46c176eb2d708f78bee81a08e61fc2ebed7107
Reviath <28-Reviath@users.noreply.git.randomchars.net>
2021-03-31 19:44:42 +0300

    Update reset_leave_channel command

--- commands/reset_leave_channel.go
+++ commands/reset_leave_channel.go
@@ -12,17 +12,6 @@ type ResetLeaveChannel struct {
 }
 
 func (r ResetLeaveChannel) Execute(ctx ctx.Ctx, session *discordgo.Session) error {
-    perms, err := session.State.UserChannelPermissions(ctx.Author().ID, ctx.Channel().ID)
-	if err == nil && (int(perms)&discordgo.PermissionAdministrator == discordgo.PermissionAdministrator) == false {
-        _, err := session.ChannelMessageSend(ctx.Channel().ID, "You need administrator permission to run this command.")
-	
-        if err != nil {
-            return nil
-        }
-
-        return err
-    }
-
     db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/remilia")
 
 	if err != nil {
@@ -33,9 +22,70 @@ func (r ResetLeaveChannel) Execute(ctx ctx.Ctx, session *discordgo.Session) erro
 
 	type Tag struct {
 		channelid string `json:"channelid"`
-	}
+        lang string `json:"language"`
+    }
 
 	var tag Tag
+
+    err = db.QueryRow("SELECT language FROM languages WHERE guildid ='" + ctx.Guild().ID + "'").Scan(&tag.lang)
+    if err == nil {
+        if tag.lang == "tr" {
+            perms, err := session.State.UserChannelPermissions(ctx.Author().ID, ctx.Channel().ID)
+            if err == nil && (int(perms)&discordgo.PermissionAdministrator == discordgo.PermissionAdministrator) == false {
+                _, err := session.ChannelMessageSend(ctx.Channel().ID, "Bu komutu kullanmak için yönetici yetkisine sahip olmalısın.")
+            
+                if err != nil {
+                    return nil
+                }
+        
+                return err
+            }
+        
+            err = db.QueryRow("SELECT channelid FROM leavechannel WHERE guildid ='" + ctx.Guild().ID + "'").Scan(&tag.channelid)
+            if err == nil {
+                delete, err := db.Query("DELETE FROM leavechannel WHERE guildid ='" + ctx.Guild().ID + "'")
+                if err != nil {
+                    _, err = session.ChannelMessageSend(ctx.Channel().ID, "Bir hata oluştu!")
+                    
+                    if err != nil {
+                        return nil
+                    }
+        
+                    return err
+                }
+        
+                defer delete.Close()
+        
+                _, err = session.ChannelMessageSend(ctx.Channel().ID, "Başarıyla çıkış kanalı sıfırlandı.")
+                
+                if err != nil {
+                    return nil
+                }
+        
+                return err
+            } else {
+                _, err = session.ChannelMessageSend(ctx.Channel().ID, "Çıkış kanalı ayarlanmamış, yani sıfırlayamazsın.")
+                
+                if err != nil {
+                    return nil
+                }
+        
+                return err
+            }
+        
+        }
+    }
+
+    perms, err := session.State.UserChannelPermissions(ctx.Author().ID, ctx.Channel().ID)
+	if err == nil && (int(perms)&discordgo.PermissionAdministrator == discordgo.PermissionAdministrator) == false {
+        _, err := session.ChannelMessageSend(ctx.Channel().ID, "You need administrator permission to run this command.")
+	
+        if err != nil {
+            return nil
+        }
+
+        return err
+    }
 
 	err = db.QueryRow("SELECT channelid FROM leavechannel WHERE guildid ='" + ctx.Guild().ID + "'").Scan(&tag.channelid)
     if err == nil {
