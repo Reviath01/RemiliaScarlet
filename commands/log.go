bc54e9b828648e8f7231147d3d1e74b5c19de8a9
Reviath <28-Reviath@users.noreply.git.randomchars.net>
2021-03-31 19:24:30 +0300

    Update log command

--- commands/log.go
+++ commands/log.go
@@ -13,6 +13,147 @@ type Log struct {
 }
 
 func (l Log) Execute(ctx ctx.Ctx, session *discordgo.Session) error {
+    db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/remilia")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer db.Close()
+
+	type Tag struct {
+		channelid string `json:"channelid"`
+        lang string `json:"language"`
+    }
+
+	var tag Tag
+
+    err = db.QueryRow("SELECT language FROM languages WHERE guildid ='" + ctx.Guild().ID + "'").Scan(&tag.lang)
+    if err == nil {
+        if tag.lang == "tr" {
+            perms, err := session.State.UserChannelPermissions(ctx.Author().ID, ctx.Channel().ID)
+            if err == nil && (int(perms)&discordgo.PermissionAdministrator == discordgo.PermissionAdministrator) == false {
+                _, err := session.ChannelMessageSend(ctx.Channel().ID, "Bu komutu kullanmak için yönetici yetkisine sahip olmalısın.")
+            
+                if err != nil {
+                    return nil
+                }
+        
+                return err
+            }
+        
+            var args string
+            if len(strings.Join(ctx.Args()," ")) < 1 {
+                _, err := session.ChannelMessageSend(ctx.Channel().ID, "Log kanalını belirtmelisin.")
+                
+                if err != nil {
+                    return nil
+                }
+                
+                return err
+            }
+            args = ctx.Args()[0]
+        
+            if len(args) < 19 {
+            c, err := session.Channel(args)
+            if err == nil {
+        
+            err = db.QueryRow("SELECT channelid FROM log WHERE guildid ='" + ctx.Guild().ID + "'").Scan(&tag.channelid)
+            if err == nil {
+            _, err = session.ChannelMessageSend(ctx.Channel().ID, "Log kanalı zaten ayarlanmış, sıfırlamak için reset_log komutunu kullan.")
+            
+            if err != nil {
+                return nil
+            }
+        
+            return err
+            } else {
+                insert, err := db.Query("INSERT INTO log (channelid, guildid) VALUES ('" + c.ID + "', '" + ctx.Guild().ID + "')")
+                if err != nil {
+                    _, err = session.ChannelMessageSend(ctx.Channel().ID, "Bir hata oluştu.")
+                    
+                    if err != nil {
+                        return nil
+                    }
+        
+                    return err
+                }
+                defer insert.Close()
+        
+                _, err = session.ChannelMessageSend(ctx.Channel().ID, "Log kanalı başarıyla ayarlandı!")
+                
+                if err != nil {
+                    return nil
+                }
+        
+                return err
+            }
+                } else {
+                    _, err = session.ChannelMessageSend(ctx.Channel().ID, "Log kanalını belirtmelisin.")
+                    
+                    if err != nil {
+                        return nil
+                    }
+        
+                    return err
+                }
+            } else {
+                if len(args) > 20 {
+                c, err := session.Channel(args[2:][:18])
+                if err == nil {
+
+            err = db.QueryRow("SELECT channelid FROM log WHERE guildid ='" + ctx.Guild().ID + "'").Scan(&tag.channelid)
+            if err == nil {
+            _, err = session.ChannelMessageSend(ctx.Channel().ID, "Log kanalı zaten ayarlanmış, sıfırlamak için reset_log komutunu kullan.")
+            
+            if err != nil {
+                return nil
+            }
+        
+            return err
+            } else {
+                insert, err := db.Query("INSERT INTO log (channelid, guildid) VALUES ('" + c.ID + "', '" + ctx.Guild().ID + "')")
+                if err != nil {
+                    _, err = session.ChannelMessageSend(ctx.Channel().ID, "Bir hata oluştu.")
+                    
+                    if err != nil {
+                        return nil
+                    }
+        
+                    return err
+                }
+                defer insert.Close()
+        
+                _, err = session.ChannelMessageSend(ctx.Channel().ID, "Log kanalı başarıyla ayarlandı.")
+                
+                if err != nil {
+                    return nil
+                }
+        
+                return err
+                }
+                } else {
+                    _, err = session.ChannelMessageSend(ctx.Channel().ID, "Log kanalını belirtmelisin.")
+                   
+                    if err != nil {
+                        return nil
+                    }
+        
+                    return err
+                }
+            } else {
+                    _, err := session.ChannelMessageSend(ctx.Channel().ID, "Log kanalını belirtmelisin.")
+                    
+                    if err != nil {
+                        return nil
+                    }
+        
+                    return err
+                }
+            }        
+        }
+    }
+
     perms, err := session.State.UserChannelPermissions(ctx.Author().ID, ctx.Channel().ID)
 	if err == nil && (int(perms)&discordgo.PermissionAdministrator == discordgo.PermissionAdministrator) == false {
         _, err := session.ChannelMessageSend(ctx.Channel().ID, "You need administrator permission to run this command.")
@@ -39,23 +180,10 @@ func (l Log) Execute(ctx ctx.Ctx, session *discordgo.Session) error {
     if len(args) < 19 {
     c, err := session.Channel(args)
     if err == nil {
-    db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/remilia")
-
-	if err != nil {
-		panic(err.Error())
-	}
-
-	defer db.Close()
-
-	type Tag struct {
-		channelid string `json:"channelid"`
-	}
-
-	var tag Tag
 
 	err = db.QueryRow("SELECT channelid FROM log WHERE guildid ='" + ctx.Guild().ID + "'").Scan(&tag.channelid)
     if err == nil {
-    _, err = session.ChannelMessageSend(ctx.Channel().ID, "Leave channel is already existing (to reset, use reset_log command).")
+    _, err = session.ChannelMessageSend(ctx.Channel().ID, "Log is already existing (to reset, use reset_log command).")
     
     if err != nil {
         return nil
@@ -96,19 +224,6 @@ func (l Log) Execute(ctx ctx.Ctx, session *discordgo.Session) error {
         if len(args) > 20 {
         c, err := session.Channel(args[2:][:18])
         if err == nil {
-    db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/remilia")
-
-	if err != nil {
-		panic(err.Error())
-	}
-
-	defer db.Close()
-
-	type Tag struct {
-		channelid string `json:"channelid"`
-	}
-
-	var tag Tag
 
 	err = db.QueryRow("SELECT channelid FROM log WHERE guildid ='" + ctx.Guild().ID + "'").Scan(&tag.channelid)
 	if err == nil {
