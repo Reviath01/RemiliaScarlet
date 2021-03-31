ecbcea7f47edcc2e7e15a4d0ac87b5b5c5b8f4e7
Reviath <28-Reviath@users.noreply.git.randomchars.net>
2021-03-31 19:36:51 +0300

    Update reset_autorole command

--- commands/reset_autorole.go
+++ commands/reset_autorole.go
@@ -13,18 +13,7 @@ type ResetAutorole struct {
 }
 
 func (r ResetAutorole) Execute(ctx ctx.Ctx, session *discordgo.Session) error {
-    perms, err := session.State.UserChannelPermissions(ctx.Author().ID, ctx.Channel().ID)
-	if err == nil && (int(perms)&discordgo.PermissionAdministrator == discordgo.PermissionAdministrator) == false {
-        _, err := session.ChannelMessageSend(ctx.Channel().ID, "You need administrator permission to run this command.")
-	
-        if err != nil {
-            return nil
-        }
-
-        return err
-    }
-
-    db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/remilia")
+       db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/remilia")
 
 	if err != nil {
 		panic(err.Error())
@@ -34,9 +23,72 @@ func (r ResetAutorole) Execute(ctx ctx.Ctx, session *discordgo.Session) error {
 
 	type Tag struct {
 		roleid string `json:"roleid"`
+        lang string `json:"language"`
 	}
 
+    var tag Tag
+
+    err = db.QueryRow("SELECT language FROM languages WHERE guildid ='" + ctx.Guild().ID + "'").Scan(&tag.lang)
+    if err == nil {
+        if tag.lang == "tr" {
+            perms, err := session.State.UserChannelPermissions(ctx.Author().ID, ctx.Channel().ID)
+	if err == nil && (int(perms)&discordgo.PermissionAdministrator == discordgo.PermissionAdministrator) == false {
+        _, err := session.ChannelMessageSend(ctx.Channel().ID, "Bu komutu kullanmak için yönetici yetkisine sahip olmalısınız.")
+	
+        if err != nil {
+            return nil
+        }
+
+        return err
+    }
+
+
 	var tag Tag
+
+	err = db.QueryRow("SELECT roleid FROM autorole WHERE guildid ='" + ctx.Guild().ID + "'").Scan(&tag.roleid)
+    if err == nil {
+        delete, err := db.Query("DELETE FROM autorole WHERE guildid ='" + ctx.Guild().ID + "'")
+        if err != nil {
+            _, err = session.ChannelMessageSend(ctx.Channel().ID, "Bir hata oluştu.")
+            
+            if err != nil {
+                return nil
+            }
+
+            return err
+        }
+
+        defer delete.Close()
+
+        _, err = session.ChannelMessageSend(ctx.Channel().ID, "Başarıyla otorol sıfırlandı.")
+        
+        if err != nil {
+            return nil
+        }
+
+        return err
+    } else {
+        _, err = session.ChannelMessageSend(ctx.Channel().ID, "Otorol ayarlanmadığı için sıfırlanamaz.")
+        
+        if err != nil {
+            return nil
+        }
+
+        return err
+    }
+        }
+    }
+
+ perms, err := session.State.UserChannelPermissions(ctx.Author().ID, ctx.Channel().ID)
+	if err == nil && (int(perms)&discordgo.PermissionAdministrator == discordgo.PermissionAdministrator) == false {
+        _, err := session.ChannelMessageSend(ctx.Channel().ID, "You need administrator permission to run this command.")
+	
+        if err != nil {
+            return nil
+        }
+
+        return err
+    }
 
 	err = db.QueryRow("SELECT roleid FROM autorole WHERE guildid ='" + ctx.Guild().ID + "'").Scan(&tag.roleid)
     if err == nil {
