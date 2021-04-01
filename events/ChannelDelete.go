812eae9a7c13456f92d55db1e102e93e2dfa30d1
Reviath <28-Reviath@users.noreply.git.randomchars.net>
2021-04-01 09:51:44 +0300

    Added Turkish support to ChannelDelete event.

--- events/ChannelDelete.go
+++ events/ChannelDelete.go
@@ -19,10 +19,34 @@ func ChannelDelete(s *discordgo.Session, event *discordgo.ChannelDelete) {
 
 	type Tag struct {
 		channelid string `json:"channelid"`
+		lang string `json:"language"`
 	}
 
 	var tag Tag
+	
+	err = db.QueryRow("SELECT language FROM languages WHERE guildid ='" + event.GuildID + "'").Scan(&tag.lang)
 
+	if err == nil {
+		if tag.lang == "tr" {
+			err = db.QueryRow("SELECT channelid FROM log WHERE guildid ='" + event.GuildID + "'").Scan(&tag.channelid)
+			if err != nil {
+				return
+			} else {
+				embed := embedutil.NewEmbed().
+					SetTitle("Kanal Silindi!").
+					AddField("Kanalın İsmi:", event.Channel.Name).
+					AddField("Kanalın İD'si:", event.Channel.ID).
+					AddField("Kanal Tipi:", string(event.Channel.Type)).
+					SetColor(0xff1000).MessageEmbed
+		
+				_, err = s.ChannelMessageSendEmbed(tag.channelid, embed)
+				if err != nil {
+					return
+				}
+			}		
+		}
+	}
+	
 	err = db.QueryRow("SELECT channelid FROM log WHERE guildid ='" + event.GuildID + "'").Scan(&tag.channelid)
 	if err != nil {
 		return
