39583e4327666498d2dd0c145a07b13d10dd6056
Reviath <28-Reviath@users.noreply.git.randomchars.net>
2021-04-01 09:54:02 +0300

    Added Turkish support to GuildMemberAdd event.

--- events/GuildMemberAdd.go
+++ events/GuildMemberAdd.go
@@ -20,10 +20,44 @@ func GuildMemberAdd(s *discordgo.Session, event *discordgo.GuildMemberAdd) {
 		channelid string `json:"channelid"`
 		roleid string `json:"roleid"`
 		welcomemessage string `json:"message"`
+		lang string `json:"language"`
 	}
 
 	var tag Tag
 	var welcomemsg string
+
+	err = db.QueryRow("SELECT language FROM languages WHERE guildid ='" + event.GuildID + "'").Scan(&tag.lang)
+
+	if err == nil {
+		if tag.lang == "tr" {
+			err = db.QueryRow("SELECT message FROM welcomemessage WHERE guildid ='" + event.GuildID + "'").Scan(&tag.welcomemessage)
+			if err != nil {
+				welcomemsg = "Sunucuya hoş geldin <@" + event.User.ID + ">!"
+			} else {
+				welcomemsg = strings.NewReplacer("{mention}", "<@" + event.User.ID + ">", "{username}", event.User.Username).Replace(tag.welcomemessage)
+			}
+
+			err = db.QueryRow("SELECT channelid FROM welcomechannel WHERE guildid ='" + event.GuildID + "'").Scan(&tag.channelid)
+			if err != nil {
+				return
+			} else {
+				channel, err := s.Channel(tag.channelid)
+				if err != nil {
+					return
+				}
+				s.ChannelMessageSend(channel.ID, welcomemsg)
+			}
+			err = db.QueryRow("SELECT roleid FROM autorole WHERE guildid ='" + event.GuildID + "'").Scan(&tag.roleid)
+			if err != nil {
+				return
+			} else {
+				err = s.GuildMemberRoleAdd(event.GuildID, event.User.ID, tag.roleid)
+				if err != nil {
+					return
+				}
+			}
+		}
+	}
 
 	err = db.QueryRow("SELECT message FROM welcomemessage WHERE guildid ='" + event.GuildID + "'").Scan(&tag.welcomemessage)
 	if err != nil {
