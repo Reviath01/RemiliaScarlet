d9a16395f3b7853665f4a27c29c6af06e1a5e2b2
Reviath <28-Reviath@users.noreply.git.randomchars.net>
2021-04-10 08:25:59 +0300

    Added client package.

---
+++ client/config.go
@@ -0,0 +1,47 @@
+package client
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+)
+
+var (
+	Token     string
+	BotPrefix string
+	Presence  string
+	Owner     string
+	config    *configStruct
+)
+
+type configStruct struct {
+	Token     string `json:"Token"`
+	BotPrefix string `json:"BotPrefix"`
+	Presence  string `json:"Presence"`
+	Owner     string `json:"Owner"`
+}
+
+func ReadConfig() error {
+	fmt.Println("Getting data from config file.")
+
+	file, err := ioutil.ReadFile("./config.json")
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+
+	err = json.Unmarshal(file, &config)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+
+	Token = config.Token
+	BotPrefix = config.BotPrefix
+	Presence = config.Presence
+	Owner = config.Owner
+
+	return nil
+}
