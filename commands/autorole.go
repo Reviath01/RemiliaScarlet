0cac59f1a23df1aba024cc8bb93bd9f75c066eb4
Reviath <28-Reviath@users.noreply.git.randomchars.net>
2021-04-14 08:26:08 +0300

    Update auto_role command

--- commands/autorole.go
+++ commands/autorole.go
@@ -30,30 +30,29 @@ func (a AutoRole) Execute(ctx ctx.Ctx, session *discordgo.Session) error {
 		if strings.Join(ctx.Args(), " ") == "" {
 			_, _ = session.ChannelMessageSend(ctx.Channel().ID, "Rolü belirtmelisin.")
 			return nil
-		} else {
-			args := multiplexer.GetRole(ctx.Args()[0])
-			if args == "Error" {
-				_, _ = session.ChannelMessageSend(ctx.Channel().ID, "Rolü belirtmelisin.")
-				return nil
-			}
-			err = db.QueryRow("SELECT roleid FROM autorole WHERE guildid ='" + ctx.Guild().ID + "'").Scan(&tag.roleid)
-			if err == nil {
-				_, _ = session.ChannelMessageSend(ctx.Channel().ID, "Otorol zaten ayarlanmış, tekrar ayarlamak için reset_autorole komutunu kullan!")
-				return nil
-			} else {
-				insert, err := db.Query("INSERT INTO autorole (roleid, guildid) VALUES ('" + args + "', '" + ctx.Guild().ID + "')")
-				if err != nil {
-					_, _ = session.ChannelMessageSend(ctx.Channel().ID, "Bir hata oluştu.")
-
-					return nil
-				}
-				defer insert.Close()
-
-				_, _ = session.ChannelMessageSend(ctx.Channel().ID, "Otorol başarıyla ayarlandı.")
-
-				return nil
-			}
 		}
+		args := multiplexer.GetRole(ctx.Args()[0])
+		if args == "Error" {
+			_, _ = session.ChannelMessageSend(ctx.Channel().ID, "Rolü belirtmelisin.")
+			return nil
+		}
+		err = db.QueryRow("SELECT roleid FROM autorole WHERE guildid ='" + ctx.Guild().ID + "'").Scan(&tag.roleid)
+		if err == nil {
+			_, _ = session.ChannelMessageSend(ctx.Channel().ID, "Otorol zaten ayarlanmış, tekrar ayarlamak için reset_autorole komutunu kullan!")
+			return nil
+		}
+		insert, err := db.Query("INSERT INTO autorole (roleid, guildid) VALUES ('" + args + "', '" + ctx.Guild().ID + "')")
+		if err != nil {
+			_, _ = session.ChannelMessageSend(ctx.Channel().ID, "Bir hata oluştu.")
+
+			return nil
+		}
+		defer insert.Close()
+
+		_, _ = session.ChannelMessageSend(ctx.Channel().ID, "Otorol başarıyla ayarlandı.")
+
+		return nil
+
 	}
 	perms, err := session.State.UserChannelPermissions(ctx.Author().ID, ctx.Channel().ID)
 	if err == nil && !(int(perms)&discordgo.PermissionAdministrator == discordgo.PermissionAdministrator) {
@@ -64,28 +63,26 @@ func (a AutoRole) Execute(ctx ctx.Ctx, session *discordgo.Session) error {
 	if strings.Join(ctx.Args(), " ") == "" {
 		_, _ = session.ChannelMessageSend(ctx.Channel().ID, "You need to specify the role.")
 		return nil
-	} else {
-		args := multiplexer.GetRole(ctx.Args()[0])
-		if args == "Error" {
-			_, _ = session.ChannelMessageSend(ctx.Channel().ID, "You need to specify the role.")
-			return nil
-		}
-		err = db.QueryRow("SELECT roleid FROM autorole WHERE guildid ='" + ctx.Guild().ID + "'").Scan(&tag.roleid)
-		if err == nil {
-			_, _ = session.ChannelMessageSend(ctx.Channel().ID, "Autorole is already existing, use reset_auto_role command to reset!")
-			return nil
-		} else {
-			insert, err := db.Query("INSERT INTO autorole (roleid, guildid) VALUES ('" + args + "', '" + ctx.Guild().ID + "')")
-			if err != nil {
-				_, _ = session.ChannelMessageSend(ctx.Channel().ID, "An error occurred.")
-
-				return nil
-			}
-			defer insert.Close()
-
-			_, _ = session.ChannelMessageSend(ctx.Channel().ID, "Successfully set autorole.")
-
-			return nil
-		}
 	}
+	args := multiplexer.GetRole(ctx.Args()[0])
+	if args == "Error" {
+		_, _ = session.ChannelMessageSend(ctx.Channel().ID, "You need to specify the role.")
+		return nil
+	}
+	err = db.QueryRow("SELECT roleid FROM autorole WHERE guildid ='" + ctx.Guild().ID + "'").Scan(&tag.roleid)
+	if err == nil {
+		_, _ = session.ChannelMessageSend(ctx.Channel().ID, "Autorole is already existing, use reset_auto_role command to reset!")
+		return nil
+	}
+	insert, err := db.Query("INSERT INTO autorole (roleid, guildid) VALUES ('" + args + "', '" + ctx.Guild().ID + "')")
+	if err != nil {
+		_, _ = session.ChannelMessageSend(ctx.Channel().ID, "An error occurred.")
+
+		return nil
+	}
+	defer insert.Close()
+
+	_, _ = session.ChannelMessageSend(ctx.Channel().ID, "Successfully set autorole.")
+
+	return nil
 }
