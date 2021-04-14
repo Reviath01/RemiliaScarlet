e8c4372fa5d28c21ba35a9563f4d2d22d9540c51
Reviath <28-Reviath@users.noreply.git.randomchars.net>
2021-04-14 07:15:26 +0300

    Added CheckLanguage function to SQL

---
+++ sql/CheckLanguage.go
@@ -0,0 +1,18 @@
+package sql
+
+func CheckLanguage(guildid string) string {
+	db := Connect()
+	type Tag struct {
+		lang string
+	}
+
+	var tag Tag
+
+	err := db.QueryRow("SELECT language FROM languages WHERE guildid ='" + guildid + "'").Scan(&tag.lang)
+
+	if err != nil {
+		return "nil"
+	} else {
+		return tag.lang
+	}
+}
