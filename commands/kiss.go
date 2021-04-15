210b26ea02795f9d9b012e247e522e30448b7609
Reviath <28-Reviath@users.noreply.git.randomchars.net>
2021-04-15 07:59:45 +0300

    Added kiss command

---
+++ commands/kiss.go
@@ -0,0 +1,64 @@
+package commands
+
+import (
+	"strings"
+
+	embedutil "git.randomchars.net/Reviath/RemiliaScarlet/EmbedUtil"
+	multiplexer "git.randomchars.net/Reviath/RemiliaScarlet/Multiplexer"
+	CommandHandler "git.randomchars.net/Reviath/RemiliaScarlet/handler"
+	"git.randomchars.net/Reviath/RemiliaScarlet/sql"
+)
+
+func KissCommand(ctx CommandHandler.Context, _ []string) error {
+	if sql.CheckLanguage(ctx.Guild.ID) == "tr" {
+		if sql.IsBlocked(ctx.Guild.ID, "kiss") == "true" {
+			ctx.Reply("Bu komut bu sunucuda engellenmiş.")
+			return nil
+		}
+
+		if len(strings.Join(multiplexer.GetArgs(ctx.Message.Content, multiplexer.GetPrefix()), " ")) < 1 {
+			ctx.Reply("Bir üye belirtmelisin.")
+
+			return nil
+		}
+
+		u, err := ctx.Session.User(multiplexer.GetUser(multiplexer.GetArgs(ctx.Message.Content, multiplexer.GetPrefix())[0]))
+		if err == nil {
+			embed := embedutil.NewEmbed().
+				SetColor(0xff1000).
+				SetDescription("<@" + ctx.Message.Author.ID + ">, <@" + u.ID + "> isimli kişiyi öptü 😘").
+				SetImage("https://media.tenor.com/images/d68747a5865b12c465e5dff31c65d5c2/tenor.gif").MessageEmbed
+			ctx.ReplyEmbed(embed)
+
+			return nil
+		} else {
+			ctx.Reply("Bir üye belirtmelisin")
+			return nil
+		}
+	}
+
+	if sql.IsBlocked(ctx.Guild.ID, "kiss") == "true" {
+		ctx.Reply("This command is blocked on this guild.")
+		return nil
+	}
+
+	if len(strings.Join(multiplexer.GetArgs(ctx.Message.Content, multiplexer.GetPrefix()), " ")) < 1 {
+		ctx.Reply("You need to specify the user.")
+
+		return nil
+	}
+
+	u, err := ctx.Session.User(multiplexer.GetUser(multiplexer.GetArgs(ctx.Message.Content, multiplexer.GetPrefix())[0]))
+	if err == nil {
+		embed := embedutil.NewEmbed().
+			SetColor(0xff1000).
+			SetDescription("<@" + ctx.Message.Author.ID + "> kisses <@" + u.ID + ">").
+			SetImage("https://media.tenor.com/images/d68747a5865b12c465e5dff31c65d5c2/tenor.gif").MessageEmbed
+		ctx.ReplyEmbed(embed)
+
+		return nil
+	} else {
+		ctx.Reply("You need to specify the user.")
+		return nil
+	}
+}
