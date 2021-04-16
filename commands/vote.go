cf5deb58c0586c7f1b65bb1b067b20a1970139a8
Reviath <28-Reviath@users.noreply.git.randomchars.net>
2021-04-16 18:33:18 +0300

    Update vote command

--- commands/vote.go
+++ commands/vote.go
@@ -1,6 +1,8 @@
 package commands
 
 import (
+	"fmt"
+
 	embedutil "git.randomchars.net/Reviath/RemiliaScarlet/EmbedUtil"
 	CommandHandler "git.randomchars.net/Reviath/RemiliaScarlet/handler"
 )
@@ -8,7 +10,7 @@ import (
 func VoteCommand(ctx CommandHandler.Context, _ []string) error {
 	voteeembed := embedutil.NewEmbed().
 		SetColor(0xc000ff).
-		SetDescription("Click [here](https://top.gg/bot/" + ctx.Session.State.User.ID + "/vote) to vote me on Top.gg!").MessageEmbed
+		SetDescription(fmt.Sprintf("Click [here](https://top.gg/bot/%s/vote) to vote me on Top.gg!", ctx.Session.State.User.ID)).MessageEmbed
 	ctx.ReplyEmbed(voteeembed)
 	return nil
 }
