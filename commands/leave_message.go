caba03399b27d6265565619fc6fec3e110454556
Reviath <28-Reviath@users.noreply.git.randomchars.net>
2021-04-16 18:28:52 +0300

    Update leave message command

--- commands/leave_message.go
+++ commands/leave_message.go
@@ -1,6 +1,7 @@
 package commands
 
 import (
+	"fmt"
 	"strings"
 
 	multiplexer "git.randomchars.net/Reviath/RemiliaScarlet/Multiplexer"
@@ -24,13 +25,13 @@ func LeaveMessageCommand(ctx CommandHandler.Context, _ []string) error {
 			return nil
 		}
 
-		err := db.QueryRow("SELECT message FROM leavemessage WHERE guildid ='" + ctx.Guild.ID + "'").Scan(&tag.message)
+		err := db.QueryRow(fmt.Sprintf("SELECT message FROM leavemessage WHERE guildid ='%s'", ctx.Guild.ID)).Scan(&tag.message)
 		if err == nil {
 			ctx.Reply("Çıkış mesajı zaten ayarlanmış, reset'lemek için reset_leave_message komutunu kullan.")
 
 			return nil
 		} else {
-			insert, err := db.Query("INSERT INTO leavemessage (message, guildid) VALUES ('" + strings.Join(multiplexer.GetArgs(ctx.Message.Content, multiplexer.GetPrefix()), " ") + "', '" + ctx.Guild.ID + "')")
+			insert, err := db.Query(fmt.Sprintf("INSERT INTO leavemessage (message, guildid) VALUES ('%s', '%s')", strings.Join(multiplexer.GetArgs(ctx.Message.Content, multiplexer.GetPrefix()), " "), ctx.Guild.ID))
 			if err != nil {
 				ctx.Reply("Bir hata oluştu.")
 				return nil
@@ -48,12 +49,12 @@ func LeaveMessageCommand(ctx CommandHandler.Context, _ []string) error {
 		return nil
 	}
 
-	err := db.QueryRow("SELECT message FROM leavemessage WHERE guildid ='" + ctx.Guild.ID + "'").Scan(&tag.message)
+	err := db.QueryRow(fmt.Sprintf("SELECT message FROM leavemessage WHERE guildid ='%s'", ctx.Guild.ID)).Scan(&tag.message)
 	if err == nil {
 		ctx.Reply("Leave message is already existing (to reset, use reset_leave_message command).")
 		return nil
 	} else {
-		insert, err := db.Query("INSERT INTO leavemessage (message, guildid) VALUES ('" + strings.Join(multiplexer.GetArgs(ctx.Message.Content, multiplexer.GetPrefix()), " ") + "', '" + ctx.Guild.ID + "')")
+		insert, err := db.Query(fmt.Sprintf("INSERT INTO leavemessage (message, guildid) VALUES ('%s', '%s')", strings.Join(multiplexer.GetArgs(ctx.Message.Content, multiplexer.GetPrefix()), " "), ctx.Guild.ID))
 		if err != nil {
 			ctx.Reply("An error occurred, please try again.")
 			return nil
