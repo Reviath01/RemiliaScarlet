7a1cfde9c8c37a2d69519595c7593faafdf4afd0
Reviath <28-Reviath@users.noreply.git.randomchars.net>
2021-04-18 20:46:35 +0300

    Update leave channel comman

--- commands/leave_channel.go
+++ commands/leave_channel.go
@@ -18,6 +18,10 @@ func LeaveChannelCommand(ctx CommandHandler.Context, _ []string) error {
 
 	var tag Tag
 	if sql.CheckLanguage(ctx.Guild.ID) == "tr" {
+		if !multiplexer.CheckAdministratorPermission(ctx.Session, ctx.Message.Author.ID, ctx.Channel.ID) {
+			ctx.Reply("Yeterli yetkiye sahip değilsin.")
+			return nil
+		}
 		if len(strings.Join(multiplexer.GetArgs(ctx.Message.Content, multiplexer.GetPrefix()), " ")) < 1 {
 			ctx.Reply("Bir kanal belirtmelisin")
 
@@ -45,7 +49,10 @@ func LeaveChannelCommand(ctx CommandHandler.Context, _ []string) error {
 			return nil
 		}
 	}
-
+	if !multiplexer.CheckAdministratorPermission(ctx.Session, ctx.Message.Author.ID, ctx.Channel.ID) {
+		ctx.Reply("You don't have enough permission.")
+		return nil
+	}
 	if len(strings.Join(multiplexer.GetArgs(ctx.Message.Content, multiplexer.GetPrefix()), " ")) < 1 {
 		ctx.Reply("You need to specify the channel.")
 
