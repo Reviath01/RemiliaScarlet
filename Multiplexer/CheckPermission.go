376e6ca3a2565268372fafdc6257b0c95cbc0fdc
Reviath <28-Reviath@users.noreply.git.randomchars.net>
2021-04-21 06:34:19 -0400

    Fixed check permission functions

--- Multiplexer/CheckPermission.go
+++ Multiplexer/CheckPermission.go
@@ -1,23 +1,25 @@
 package multiplexer
 
-import "github.com/bwmarrin/discordgo"
+import (
+	"github.com/bwmarrin/discordgo"
+)
 
 func CheckBanPermission(s *discordgo.Session, userID string, channelID string) bool {
-	p, err := s.State.UserChannelPermissions(userID, channelID)
-	return (err == nil) && (p&discordgo.PermissionBanMembers == discordgo.PermissionBanMembers)
+	p, _ := s.UserChannelPermissions(userID, channelID)
+	return p&discordgo.PermissionBanMembers == discordgo.PermissionBanMembers
 }
 
 func CheckAdministratorPermission(s *discordgo.Session, userID string, channelID string) bool {
-	p, err := s.State.UserChannelPermissions(userID, channelID)
-	return (err == nil) && (p&discordgo.PermissionAdministrator == discordgo.PermissionAdministrator)
+	p, _ := s.UserChannelPermissions(userID, channelID)
+	return p&discordgo.PermissionAdministrator == discordgo.PermissionAdministrator
 }
 
 func CheckManageMessagesPermission(s *discordgo.Session, userID string, channelID string) bool {
-	p, err := s.State.UserChannelPermissions(userID, channelID)
-	return (err == nil) && (p&discordgo.PermissionManageMessages == discordgo.PermissionManageMessages)
+	p, _ := s.UserChannelPermissions(userID, channelID)
+	return p&discordgo.PermissionManageMessages == discordgo.PermissionManageMessages
 }
 
 func CheckKickPermission(s *discordgo.Session, userID string, channelID string) bool {
-	p, err := s.State.UserChannelPermissions(userID, channelID)
-	return (err == nil) && (p&discordgo.PermissionKickMembers == discordgo.PermissionKickMembers)
+	p, _ := s.UserChannelPermissions(userID, channelID)
+	return p&discordgo.PermissionKickMembers == discordgo.PermissionKickMembers
 }
