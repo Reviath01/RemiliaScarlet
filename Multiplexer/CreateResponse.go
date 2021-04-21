cef95e732a0ec12108ef555a35d7c450c59d485f
Reviath <28-Reviath@users.noreply.git.randomchars.net>
2021-04-21 08:09:17 -0400

    Creating embed response made easy with multiplexer.

--- Multiplexer/CreateResponse.go
+++ Multiplexer/CreateResponse.go
@@ -1,6 +1,9 @@
 package multiplexer
 
-import "git.randomchars.net/Reviath/RemiliaScarlet/interactions"
+import (
+	"git.randomchars.net/Reviath/RemiliaScarlet/interactions"
+	"github.com/bwmarrin/discordgo"
+)
 
 func CreateResponse(message string) interactions.InteractionResponse {
 	response := interactions.InteractionResponse{
@@ -8,6 +11,17 @@ func CreateResponse(message string) interactions.InteractionResponse {
 		Data: interactions.InteractionResponseData{
 			TTS:     false,
 			Content: message,
+		},
+	}
+	return response
+}
+
+func CreateEmbedResponse(embed *discordgo.MessageEmbed) interactions.InteractionResponse {
+	response := interactions.InteractionResponse{
+		Type: interactions.InteractionResponseTypeChannelMessageWithSource,
+		Data: interactions.InteractionResponseData{
+			TTS:    false,
+			Embeds: []discordgo.MessageEmbed{*embed},
 		},
 	}
 	return response
