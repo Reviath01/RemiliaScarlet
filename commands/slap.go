648d5d8ca7adffbbf4847aa17c81da2c0157382f
Reviath <28-Reviath@users.noreply.git.randomchars.net>
2021-04-27 18:50:58 +0300

    Slap command is ready to use with FreeNitori's embed util

--- commands/slap.go
+++ commands/slap.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"strings"
 
-	embedutil "git.randomchars.net/Reviath/RemiliaScarlet/EmbedUtil"
+	embedutil "git.randomchars.net/FreeNitori/EmbedUtil"
 	multiplexer "git.randomchars.net/Reviath/RemiliaScarlet/Multiplexer"
 	CommandHandler "git.randomchars.net/Reviath/RemiliaScarlet/handler"
 	"git.randomchars.net/Reviath/RemiliaScarlet/sql"
@@ -24,12 +24,10 @@ func SlapCommand(ctx CommandHandler.Context, _ []string) error {
 
 		u, err := ctx.Session.User(multiplexer.GetUser(multiplexer.GetArgs(ctx.Message.Content, multiplexer.GetPrefix())[0]))
 		if err == nil {
-			embed := embedutil.NewEmbed().
-				SetColor(0xff1000).
-				SetDescription(fmt.Sprintf("<@%s>, <@%s> isimli kişiye vurdu", ctx.Message.Author.ID, u.ID)).
-				SetImage("https://images-ext-1.discordapp.net/external/79sCWyD-TmmyjFxlaQIxAkAANAfV529d-LDHNkGDM0M/%3Fitemid%3D10426943/https/media1.tenor.com/images/b6d8a83eb652a30b95e87cf96a21e007/tenor.gif").MessageEmbed
+			embed := embedutil.New("", fmt.Sprintf("<@%s>, <@%s> isimli kişiye vurdu", ctx.Message.Author.ID, u.ID))
+			embed.Color = 0xff1000
+			embed.SetImage("https://images-ext-1.discordapp.net/external/79sCWyD-TmmyjFxlaQIxAkAANAfV529d-LDHNkGDM0M/%3Fitemid%3D10426943/https/media1.tenor.com/images/b6d8a83eb652a30b95e87cf96a21e007/tenor.gif")
 			ctx.ReplyEmbed(embed)
-
 			return nil
 		}
 		ctx.Reply("Bir üye belirtmelisin.")
@@ -48,10 +46,9 @@ func SlapCommand(ctx CommandHandler.Context, _ []string) error {
 
 	u, err := ctx.Session.User(multiplexer.GetUser(multiplexer.GetArgs(ctx.Message.Content, multiplexer.GetPrefix())[0]))
 	if err == nil {
-		embed := embedutil.NewEmbed().
-			SetColor(0xff1000).
-			SetDescription(fmt.Sprintf("<@%s> slaps <@%s>", ctx.Message.Author.ID, u.ID)).
-			SetImage("https://images-ext-1.discordapp.net/external/79sCWyD-TmmyjFxlaQIxAkAANAfV529d-LDHNkGDM0M/%3Fitemid%3D10426943/https/media1.tenor.com/images/b6d8a83eb652a30b95e87cf96a21e007/tenor.gif").MessageEmbed
+		embed := embedutil.New("", fmt.Sprintf("<@%s> slaps <@%s>", ctx.Message.Author.ID, u.ID))
+		embed.Color = 0xff1000
+		embed.SetImage("https://images-ext-1.discordapp.net/external/79sCWyD-TmmyjFxlaQIxAkAANAfV529d-LDHNkGDM0M/%3Fitemid%3D10426943/https/media1.tenor.com/images/b6d8a83eb652a30b95e87cf96a21e007/tenor.gif")
 		ctx.ReplyEmbed(embed)
 		return nil
 	}
