1c6fa63340ef39321830404fe22d58ceb40370ca
Reviath <28-Reviath@users.noreply.git.randomchars.net>
2021-04-27 16:57:10 +0300

    Invite command is ready to use with FreeNitori's embed util

--- commands/invite.go
+++ commands/invite.go
@@ -3,14 +3,13 @@ package commands
 import (
 	"fmt"
 
-	embedutil "git.randomchars.net/Reviath/RemiliaScarlet/EmbedUtil"
+	embedutil "git.randomchars.net/FreeNitori/EmbedUtil"
 	CommandHandler "git.randomchars.net/Reviath/RemiliaScarlet/handler"
 )
 
 func InviteCommand(ctx CommandHandler.Context, _ []string) error {
-	inviteembed := embedutil.NewEmbed().
-		SetColor(0xc000ff).
-		SetDescription(fmt.Sprintf("Click [here](https://discord.com/oauth2/authorize?client_id=%s&scope=applications.commands+bot&permissions=8) to invite me!", ctx.Session.State.User.ID)).MessageEmbed
+	inviteembed := embedutil.New("", fmt.Sprintf("Click [here](https://discord.com/oauth2/authorize?client_id=%s&scope=applications.commands+bot&permissions=8) to invite me!", ctx.Session.State.User.ID))
+	inviteembed.Color = 0xc000ff
 	ctx.ReplyEmbed(inviteembed)
 
 	return nil
