c00e5459b259ae63ae31ea535f099b42e6aaf922
Reviath <28-Reviath@users.noreply.git.randomchars.net>
2021-04-27 17:00:53 +0300

    Issue command is ready to use with FreeNitori's embed util

--- commands/issue.go
+++ commands/issue.go
@@ -1,26 +1,23 @@
 package commands
 
 import (
-	embedutil "git.randomchars.net/Reviath/RemiliaScarlet/EmbedUtil"
+	embedutil "git.randomchars.net/FreeNitori/EmbedUtil"
 	CommandHandler "git.randomchars.net/Reviath/RemiliaScarlet/handler"
 	"git.randomchars.net/Reviath/RemiliaScarlet/sql"
 )
 
 func IssueCommand(ctx CommandHandler.Context, _ []string) error {
 	if sql.CheckLanguage(ctx.Guild.ID) == "tr" {
-		issueembed := embedutil.NewEmbed().
-			SetColor(0xffa935).
-			SetDescription("GitLab üzerinden bir issue oluşturmak için [buraya](https://git.randomchars.net/Reviath/RemiliaScarlet/-/issues/new) tıkla!").
-			AddField("Eğer GitLab kullanmayı bilmiyorsan,", "[Sunucumuza](https://discord.gg/FshmaUh9eV) gelip sorunu belirtebilirsin.").MessageEmbed
+		issueembed := embedutil.New("", "GitLab üzerinden bir issue oluşturmak için [buraya](https://git.randomchars.net/Reviath/RemiliaScarlet/-/issues/new) tıkla!")
+		issueembed.Color = 0xffa935
+		issueembed.AddField("Eğer GitLab kullanmayı bilmiyorsan,", "[Sunucumuza](https://discord.gg/FshmaUh9eV) gelip sorunu belirtebilirsin.", true)
 		ctx.ReplyEmbed(issueembed)
-
 		return nil
 	}
-	issueembed := embedutil.NewEmbed().
-		SetColor(0xffa935).
-		SetDescription("Click [here](https://git.randomchars.net/Reviath/RemiliaScarlet/-/issues/new) to create an issue on GitLab").
-		AddField("If you don't know how to use GitLab,", "You can come to our [guild](https://discord.gg/FshmaUh9eV) and specify the problem.").MessageEmbed
-	ctx.ReplyEmbed(issueembed)
 
+	issueembed := embedutil.New("", "Click [here](https://git.randomchars.net/Reviath/RemiliaScarlet/-/issues/new) to create an issue on GitLab")
+	issueembed.Color = 0xffa935
+	issueembed.AddField("If you don't know how to use GitLab,", "You can come to our [guild](https://discord.gg/FshmaUh9eV) and specify the problem.", true)
+	ctx.ReplyEmbed(issueembed)
 	return nil
 }
