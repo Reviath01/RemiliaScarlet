d118d93dd808fb1a348cacc49524eed5e9545518
Reviath <28-Reviath@users.noreply.git.randomchars.net>
2021-04-28 10:35:13 +0300

    Added GuildRoleCreate event.

---
+++ events/GuildRoleCreate.go
@@ -0,0 +1,47 @@
+package events
+
+import (
+	"strconv"
+
+	embedutil "git.randomchars.net/FreeNitori/EmbedUtil"
+	"git.randomchars.net/Reviath/RemiliaScarlet/sql"
+	"github.com/bwmarrin/discordgo"
+)
+
+func GuildRoleCreate(s *discordgo.Session, event *discordgo.GuildRoleCreate) {
+	db := sql.Connect()
+	defer db.Close()
+
+	type Tag struct {
+		channelid string
+	}
+
+	var tag Tag
+
+	if sql.CheckLanguage(event.GuildID) == "tr" {
+		err := db.QueryRow("SELECT channelid FROM log WHERE guildid ='" + event.GuildID + "'").Scan(&tag.channelid)
+		if err != nil {
+			return
+		}
+		embed := embedutil.New("Rol Oluşturuldu!", "")
+		embed.AddField("Rolün İsmi:", event.Role.Name+" ( <@&"+event.Role.ID+"> )", true)
+		embed.AddField("Rol İD'si:", event.Role.ID, true)
+		embed.AddField("Rol Rengi:", strconv.Itoa(event.Role.Color), true)
+		embed.Color = 0xefff00
+
+		s.ChannelMessageSendEmbed(tag.channelid, embed.MessageEmbed)
+		return
+	}
+
+	err := db.QueryRow("SELECT channelid FROM log WHERE guildid ='" + event.GuildID + "'").Scan(&tag.channelid)
+	if err != nil {
+		return
+	}
+	embed := embedutil.New("Role Created!", "")
+	embed.AddField("Role Name:", event.Role.Name+" ( <@&"+event.Role.ID+"> )", true)
+	embed.AddField("Role ID:", event.Role.ID, true)
+	embed.AddField("Role Color:", strconv.Itoa(event.Role.Color), true)
+	embed.Color = 0xefff00
+
+	s.ChannelMessageSendEmbed(tag.channelid, embed.MessageEmbed)
+}
