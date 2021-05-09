ee1f0e9fa97f7c68fc0e3daccf211ef59bbcad4f
Reviath <28-Reviath@users.noreply.git.randomchars.net>
2021-05-09 10:32:41 +0300

    Using switch statement instead of if / else statements on author command.

--- commands/author.go
+++ commands/author.go
@@ -1,41 +1,19 @@
 package commands
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
 
 	embedutil "git.randomchars.net/FreeNitori/EmbedUtil"
+	"git.randomchars.net/Reviath/RemiliaScarlet/config"
 	CommandHandler "git.randomchars.net/Reviath/RemiliaScarlet/handler"
 	"git.randomchars.net/Reviath/RemiliaScarlet/sql"
 )
 
 func AuthorCommand(ctx CommandHandler.Context, _ []string) error {
-	file, err := ioutil.ReadFile("./config.json")
+	config.ReadConfig()
 
-	if err != nil {
-		return nil
-	}
-
-	type configStruct struct {
-		Token     string `json:"Token"`
-		BotPrefix string `json:"BotPrefix"`
-		Presence  string `json:"Presence"`
-		Owner     string `json:"Owner"`
-	}
-
-	var (
-		config *configStruct
-	)
-
-	err = json.Unmarshal(file, &config)
-
-	if err != nil {
-		fmt.Println(err.Error())
-		return nil
-	}
-
-	if sql.CheckLanguage(ctx.Guild.ID) == "tr" {
+	switch sql.CheckLanguage(ctx.Guild.ID) {
+	case "tr":
 		if sql.IsBlocked(ctx.Guild.ID, "author") == "true" {
 			_, _ = ctx.Reply("Bu komut bu sunucuda engellenmiş.")
 			return nil
@@ -53,22 +31,23 @@ func AuthorCommand(ctx CommandHandler.Context, _ []string) error {
 		ctx.ReplyEmbed(authorembed)
 
 		return nil
-	}
-	if sql.IsBlocked(ctx.Guild.ID, "author") == "true" {
-		_, _ = ctx.Reply("This command is blocked on this guild.")
+	default:
+		if sql.IsBlocked(ctx.Guild.ID, "author") == "true" {
+			_, _ = ctx.Reply("This command is blocked on this guild.")
+			return nil
+		}
+
+		u, err := ctx.Session.User(config.Owner)
+
+		if err != nil {
+			return nil
+		}
+
+		authorembed := embedutil.New("", "")
+		authorembed.Color = 0x007bff
+		authorembed.AddField("My Author:", fmt.Sprintf("<@%s> ([%s#%s](https://discord.com/users/%s))", u.ID, u.Username, u.Discriminator, u.ID), true)
+		ctx.ReplyEmbed(authorembed)
+
 		return nil
 	}
-
-	u, err := ctx.Session.User(config.Owner)
-
-	if err != nil {
-		return nil
-	}
-
-	authorembed := embedutil.New("", "")
-	authorembed.Color = 0x007bff
-	authorembed.AddField("My Author:", fmt.Sprintf("<@%s> ([%s#%s](https://discord.com/users/%s))", u.ID, u.Username, u.Discriminator, u.ID), true)
-	ctx.ReplyEmbed(authorembed)
-
-	return nil
 }
