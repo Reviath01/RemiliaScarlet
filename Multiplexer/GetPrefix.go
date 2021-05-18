cab279ce5ba55c439a956912e62dab2a6fe4b75f
Reviath <28-Reviath@users.noreply.git.randomchars.net>
2021-05-18 20:43:17 +0300

    Deleted unnecessary stuff from Multiplexer.

--- Multiplexer/GetPrefix.go
+++ Multiplexer/GetPrefix.go
@@ -1,13 +1,6 @@
 package multiplexer
 
-var (
-	BotPrefix string        // BotPrefix is bot's prefix to cut prefix's length
-	config    *configStruct // config structure
-)
-
-type configStruct struct {
-	BotPrefix string `json:"BotPrefix"`
-}
+import "git.randomchars.net/Reviath/RemiliaScarlet/config"
 
 // GetPrefix returns to Bot's prefix from config package
 func GetPrefix() string {
