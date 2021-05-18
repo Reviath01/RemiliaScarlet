97e83673abcfa95f0edc35541c6bedc794041c6d
Reviath <28-Reviath@users.noreply.git.randomchars.net>
2021-05-18 21:15:21 +0300

    Updated issue command and issue slash command.

--- InteractionCommands/issue.go
+++ InteractionCommands/issue.go
@@ -1,8 +1,11 @@
 package interactioncommands
 
 import (
+	"fmt"
+
 	embedutil "git.randomchars.net/FreeNitori/EmbedUtil"
 	multiplexer "git.randomchars.net/Reviath/RemiliaScarlet/Multiplexer"
+	"git.randomchars.net/Reviath/RemiliaScarlet/config"
 	"git.randomchars.net/Reviath/RemiliaScarlet/interactions"
 	"git.randomchars.net/Reviath/RemiliaScarlet/sql"
 	"github.com/bwmarrin/discordgo"
@@ -10,16 +13,18 @@ import (
 
 // IssueCommand is issue command for interactions
 func IssueCommand(session *discordgo.Session, interaction interactions.Interaction) interactions.InteractionResponse {
+	config.ReadConfig()
+	u, _ := session.User(config.Owner)
 	switch sql.CheckLanguage(interaction.GuildID) {
 	case "tr":
 		issueembed := embedutil.New("", "GitLab üzerinden bir issue oluşturmak için [buraya](https://git.randomchars.net/Reviath/RemiliaScarlet/-/issues/new) tıkla!")
 		issueembed.Color = 0xffa935
-		issueembed.AddField("Eğer GitLab kullanmayı bilmiyorsan,", "[Sunucumuza](https://discord.gg/FshmaUh9eV) gelip sorunu belirtebilirsin.", true)
+		issueembed.AddField("Eğer GitLab kullanmayı bilmiyorsan,", fmt.Sprintf("Sahibime DM yoluyla ulaşabilirsiniz [%s#%s](https://discord.com/users/%s)", u.Username, u.Discriminator, u.ID), true)
 		return multiplexer.CreateEmbedResponse(issueembed)
 	default:
 		issueembed := embedutil.New("", "Click [here](https://git.randomchars.net/Reviath/RemiliaScarlet/-/issues/new) to create an issue on GitLab")
 		issueembed.Color = 0xffa935
-		issueembed.AddField("If you don't know how to use GitLab,", "You can come to our [guild](https://discord.gg/FshmaUh9eV) and specify the problem.", true)
+		issueembed.AddField("If you don't know how to use GitLab,", fmt.Sprintf("You can write from DM to my owner [%s#%s](https://discord.com/users/%s)", u.Username, u.Discriminator, u.ID), true)
 		return multiplexer.CreateEmbedResponse(issueembed)
 	}
 }
--- commands/issue.go
+++ commands/issue.go
@@ -1,18 +1,22 @@
 package commands
 
 import (
+	"fmt"
+
 	embedutil "git.randomchars.net/FreeNitori/EmbedUtil"
+	"git.randomchars.net/Reviath/RemiliaScarlet/config"
 	CommandHandler "git.randomchars.net/Reviath/RemiliaScarlet/handler"
 	"git.randomchars.net/Reviath/RemiliaScarlet/sql"
 )
 
 // IssueCommand is a handler for issue command
 func IssueCommand(ctx CommandHandler.Context, _ []string) error {
+	u, _ := ctx.Session.User(config.Owner)
 	switch sql.CheckLanguage(ctx.Guild.ID) {
 	case "tr":
 		issueembed := embedutil.New("", "GitLab üzerinden bir issue oluşturmak için [buraya](https://git.randomchars.net/Reviath/RemiliaScarlet/-/issues/new) tıkla!")
 		issueembed.Color = 0xffa935
-		issueembed.AddField("Eğer GitLab kullanmayı bilmiyorsan,", "[Sunucumuza](https://discord.gg/FshmaUh9eV) gelip sorunu belirtebilirsin.", true)
+		issueembed.AddField("Eğer GitLab kullanmayı bilmiyorsan,", fmt.Sprintf("Sahibime DM yoluyla ulaşabilirsiniz [%s#%s](https://discord.com/users/%s)", u.Username, u.Discriminator, u.ID), true)
 		ctx.ReplyEmbed(issueembed)
 		return nil
 
@@ -20,7 +24,7 @@ func IssueCommand(ctx CommandHandler.Context, _ []string) error {
 
 		issueembed := embedutil.New("", "Click [here](https://git.randomchars.net/Reviath/RemiliaScarlet/-/issues/new) to create an issue on GitLab")
 		issueembed.Color = 0xffa935
-		issueembed.AddField("If you don't know how to use GitLab,", "You can come to our [guild](https://discord.gg/FshmaUh9eV) and specify the problem.", true)
+		issueembed.AddField("If you don't know how to use GitLab,", fmt.Sprintf("You can write from DM to my owner [%s#%s](https://discord.com/users/%s)", u.Username, u.Discriminator, u.ID), true)
 		ctx.ReplyEmbed(issueembed)
 		return nil
 	}
