3f378d9b995633e538163a40aef5a160d3c00c6c
Reviath <28-Reviath@users.noreply.git.randomchars.net>
2021-05-18 21:24:16 +0300

    Fixed GetPrefix function.

--- Multiplexer/GetPrefix.go
+++ Multiplexer/GetPrefix.go
@@ -4,5 +4,6 @@ import "git.randomchars.net/Reviath/RemiliaScarlet/config"
 
 // GetPrefix returns to Bot's prefix from config package
 func GetPrefix() string {
+	config.ReadConfig()
 	return config.BotPrefix
 }
