e78e24b303ce4b22d41a009a217c5fc2795222f1
Reviath <28-Reviath@users.noreply.git.randomchars.net>
2021-05-18 20:41:53 +0300

    Fixed GetPrefix function at multiplexer.

--- Multiplexer/GetPrefix.go
+++ Multiplexer/GetPrefix.go
@@ -1,11 +1,5 @@
 package multiplexer
 
-import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-)
-
 var (
 	BotPrefix string        // BotPrefix is bot's prefix to cut prefix's length
 	config    *configStruct // config structure
@@ -15,23 +9,7 @@ type configStruct struct {
 	BotPrefix string `json:"BotPrefix"`
 }
 
-// GetPrefix returns to Bot's prefix from config.json file
+// GetPrefix returns to Bot's prefix from config package
 func GetPrefix() string {
-	file, err := ioutil.ReadFile("./config.json")
-
-	if err != nil {
-		fmt.Println(err.Error())
-		return "err"
-	}
-
-	err = json.Unmarshal(file, &config)
-
-	if err != nil {
-		fmt.Println(err.Error())
-		return "err"
-	}
-
-	BotPrefix = config.BotPrefix
-
-	return BotPrefix
+	return config.BotPrefix
 }
